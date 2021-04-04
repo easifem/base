a9209e4f0ffc4953ce95b71f0d52b366c9a4d386
Vikas Sharma <vickysharma0812@gmail.com>
2021-04-04 15:02:33 +0530

    :wo install.py
    working on install.py

--- install.py
+++ install.py
@@ -9,13 +9,17 @@ import platform
 
 
 def installRequest(LIB):
+    while True:
+        choice = input(f"Do you want to Install {LIB} 'yes' or 'no' [Y/n]: ").lower()
+        if choice in ['Y', 'y', 'ye', 'yes']:
+          return True
+        else:
+          return False
+
+def getOption(key, opt):
   while True:
-    choice = input(
-        f"Do you want to Install {LIB} 'yes' or 'no' [Y/n]: ").lower()
-    if choice in ['Y', 'y', 'ye', 'yes']:
-      return True
-    else:
-      return False
+    separator = ', '
+    return input( f"select option for {key}, possible options are : {separator.join(opt)} : ") + " "
 
 print("Detecting OS type...")
 _os = platform.system()
@@ -25,10 +29,29 @@ if _os == 'Windows':
     #print("Please use Windows Subsystem Linux(WSL) ")
     #print("Installation DONE!!")
 else:
-    if installRequest('OpenMP'):
-      cmake_def = '-DUSE_OpenMP=ON'
-    cmake_def = cmake_def + " -DBUILD_TYPE='Release' -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=${EASIFEM_BASE} -DUSE_Int32=ON -DUSE_Real64=ON"
+    if(installRequest("OpenBLAS")):
+      cwd = os.getcwd()
+      os.chdir(os.getenv('EASIFEM_EXTPKGS'))
+      os.system(f"git clone --branch develop https://github.com/xianyi/OpenBLAS.git")
+      os.chdir(os.getenv('EASIFEM_EXTPKGS') + "/OpenBLAS")
+      openblas_def = "-S ./ -B build -DCMAKE_INSTALL_PREFIX=${EASIFEM_EXTPKGS} -DBUILD_WITHOUT_LAPACK=OFF -DBUILD_WITHOUT_CBLAS=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DNOFORTRAN=OFF"
+      os.system( f"cmake {openblas_def}")
+      os.chdir(cwd)
+
+    cmake_def = ""
+    opt = getOption("USE_OpenMP", ["ON", "OFF"] )
+    cmake_def += "-DUSE_OpenMP=" + opt
+
+    cmake_def += "-DBUILD_TYPE=" + getOption("BUILD_TYPE", ["Release", "Debug"])
+
+    cmake_def += "-DBUILD_SHARED_LIBS=" + getOption("BUILD_SHARED_LIBS", ["ON", "OFF"])
+
+    cmake_def += " -DCMAKE_INSTALL_PREFIX=${EASIFEM_BASE} "
+
+    cmake_def += " -DUSE_Int32=ON -DUSE_Real64=ON "
+
     print( "CMAKE DEF : ", cmake_def )
-    os.system( f"cmake -S ./ -B ./build {cmake_def}")
-    os.system(f"cmake --build ./build --target install" )
+
+    # os.system( f"cmake -S ./ -B ./build {cmake_def}")
+    # os.system(f"cmake --build ./build --target install" )
     print("Installation DONE!!")
