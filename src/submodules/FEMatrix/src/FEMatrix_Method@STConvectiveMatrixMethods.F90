ebec3feaf34b844976ffeb3b40a8763ea2c24232
Vikas <vickysharma0812@gmail.com>
2021-12-04 22:04:56 +0530

    working on convective matrix

--- src/submodules/FEMatrix/src/FEMatrix_Method@STConvectiveMatrixMethods.F90
+++ src/submodules/FEMatrix/src/FEMatrix_Method@STConvectiveMatrixMethods.F90
@@ -20,21 +20,274 @@ USE BaseMethod
 IMPLICIT NONE
 CONTAINS
 
+#include "./STCM_1.inc"
+#include "./STCM_2.inc"
+#include "./STCM_3.inc"
+#include "./STCM_4.inc"
+#include "./STCM_5.inc"
+#include "./STCM_6.inc"
+#include "./STCM_7.inc"
+#include "./STCM_8.inc"
+
 !----------------------------------------------------------------------------
 !                                                         STConvectiveMatrix
 !----------------------------------------------------------------------------
-
 MODULE PROCEDURE Mat4_STConvectiveMatrix_1
-#include "./Mat4_STConvectiveMatrix_1.inc"
+
+INTEGER(I4B) :: rankC
+
+IF (PRESENT(c)) THEN
+  rankC = .rank.c
+ELSE
+  rankC = SCALAR
+END IF
+!!
+SELECT CASE (term1)
+!!
+CASE (DEL_NONE)
+  SELECT CASE (term2)
+!!
+  CASE (DEL_X_ALL)
+!!
+!! term1 = none
+!! term2 = del_x_all
+!! c = vector
+!!
+    IF (rankC .EQ. VECTOR) THEN
+      CALL STCM_1(ans=ans, test=test, trial=trial, &
+          & term1=term1, term2=term2, c=c, projectOn=projectOn)
+!!
+!! term1 = none
+!! term2 = del_x_all
+!! c = scalar
+!!
+    ELSEIF (rankC .EQ. SCALAR) THEN
+      !! none_all_scalar_1()
+      CALL STCM_3(ans=ans, test=test, trial=trial, &
+          & term1=term1, term2=term2, c=c)
+    END IF
+!!
+  CASE (DEL_X, DEL_Y, DEL_Z)
+!!
+!! term1 = none
+!! term2 = dx/dy/dz
+!! c = vector
+!!
+    IF (rankC .EQ. VECTOR) THEN
+      CALL STCM_1(ans=ans, test=test, trial=trial, &
+          & term1=term1, term2=term2, c=c, projectOn=projectOn)
+!!
+!! term1 = none
+!! term2 = dx/dy/dz
+!! c = scalar
+!!
+    ELSEIF (rankC .EQ. SCALAR) THEN
+      CALL STCM_2(ans=ans, test=test, trial=trial, &
+          & term1=term1, term2=term2, dim=term2, c=c)
+    END IF
+  END SELECT
+!!
+!!
+!!
+CASE (DEL_X, DEL_Y, DEL_Z)
+!!
+  SELECT CASE (term2)
+!!
+  CASE (DEL_NONE)
+!!
+!! term1 = dx/dy/dz
+!! term2 = none
+!! c = vector
+!!
+    IF (rankC .EQ. VECTOR) THEN
+      CALL STCM_1(ans=ans, test=test, trial=trial, &
+          & term1=term1, term2=term2, c=c, projectOn=projectOn)
+!!
+!! term1 = dx/dy/dz
+!! term2 = none
+!! c = scalar
+!!
+    ELSEIF (rankC .EQ. SCALAR) THEN
+      CALL STCM_2(ans=ans, test=test, trial=trial, &
+           & term1=term1, term2=term2, dim=term1, c=c)
+    END IF
+!!
+  CASE (DEL_t)
+!!
+!! term1 = dx/dy/dz
+!! term2 = dt
+!! c = vector
+!!
+    IF (rankC .EQ. VECTOR) THEN
+      CALL STCM_8(ans=ans, test=test, trial=trial, c=c, projectOn='test')
+!!
+!! term1 = dx/dy/dz
+!! term2 = dt
+!! c = scalar
+!!
+    ELSEIF (rankC .EQ. SCALAR) THEN
+      CALL STCM_4(ans=ans, test=test, trial=trial, &
+           & term1=term1, term2=term2, dim=term1, c=c)
+    END IF
+!!
+  CASE (DEL_x, DEL_y, DEL_z)
+!!
+!! term1 = dx/dy/dz
+!! term2 = dx/dy/dz
+!! c = vector
+!!
+    IF (rankC .EQ. VECTOR) THEN
+      CALL STCM_6(ans=ans, test=test, trial=trial, &
+           & term1=term1, term2=term2, c=c, projectOn=projectOn)
+!!
+!! term1 = dx/dy/dz
+!! term2 = dx/dy/dz
+!! c = scalar
+!!
+    ELSEIF (rankC .EQ. SCALAR) THEN
+       !! TODO
+    END IF
+
+  CASE (DEL_x_all)
+!!
+!! term1 = dx/dy/dz
+!! term2 = del_x_all
+!! c = vector
+!!
+    IF (rankC .EQ. VECTOR) THEN
+      CALL STCM_7(ans=ans, test=test, trial=trial, &
+           & term1=term1, term2=term2, c=c, projectOn="test")
+!!
+!! term1 = dx/dy/dz
+!! term2 = del_x_all
+!! c = scalar
+!!
+    ELSEIF (rankC .EQ. SCALAR) THEN
+        !! TODO
+    END IF
+
+  END SELECT
+!!
+!!
+!!
+CASE (DEL_X_ALL)
+!!
+  SELECT CASE (term2)
+  CASE (DEL_NONE)
+!!
+!! term1 = del_x_all
+!! term2 = none
+!! c = vector
+!!
+    IF (rankC .EQ. VECTOR) THEN
+      CALL STCM_1(ans=ans, test=test, trial=trial, &
+          & term1=term1, term2=term2, c=c, projectOn=projectOn)
+!!
+!! term1 = del_x_all
+!! term2 = none
+!! c = scalar
+!!
+    ELSEIF (rankC .EQ. SCALAR) THEN
+      CALL STCM_3(ans=ans, test=test, trial=trial, &
+          & term1=term1, term2=term2, c=c)
+    END IF
+!!
+  CASE (DEL_t)
+!!
+!! term1 = del_x_all
+!! term2 = dt
+!! c = vector
+    IF (rankC .EQ. VECTOR) THEN
+      CALL STCM_8(ans=ans, test=test, trial=trial, c=c, projectOn='test')
+!!
+!! term1 = del_x_all
+!! term2 = dt
+!! c = scalar
+!!
+    ELSEIF (rankC .EQ. SCALAR) THEN
+      CALL STCM_5(ans=ans, test=test, trial=trial, &
+          & term1=term1, term2=term2, c=c)
+    END IF
+!!
+  CASE (DEL_x, DEL_y, DEL_z)
+!!
+!! term1 = del_x_all
+!! term2 = dx/dy/dz
+!! c = vector
+!!
+    IF (rankC .EQ. VECTOR) THEN
+      CALL STCM_7(ans=ans, test=test, trial=trial, &
+           & term1=term1, term2=term2, c=c, projectOn="trial")
+!!
+!! term1 = del_x_all
+!! term2 = dx/dy/dz
+!! c = scalar
+!! TODO
+    ELSEIF (rankC .EQ. SCALAR) THEN
+    END IF
+!!
+  CASE (DEL_x_all)
+!!
+!! term1 = del_x_all
+!! term2 = del_x_all
+!! c = vector
+!!
+    IF (rankC .EQ. VECTOR) THEN
+      CALL STCM_7(ans=ans, test=test, trial=trial, &
+           & term1=term1, term2=term2, c=c, projectOn="trial")
+!!
+!! term1 = del_x_all
+!! term2 = del_x_all
+!! c = scalar
+!! TODO
+    ELSEIF (rankC .EQ. SCALAR) THEN
+    END IF
+  END SELECT
+!!
+!!
+CASE (DEL_t)
+!!
+  SELECT CASE (term2)
+!!
+  CASE (DEL_X, DEL_Y, DEL_Z)
+!!
+!! term1 = dt
+!! term2 = dx/dy/dz
+!! c = vector
+!!
+    IF (rankC .EQ. VECTOR) THEN
+      CALL STCM_8(ans=ans, test=test, trial=trial, c=c, projectOn='trial')
+!!
+!! term1 = dt
+!! term2 = dx/dy/dz
+!! c = scalar
+!!
+    ELSEIF (rankC .EQ. SCALAR) THEN
+      CALL STCM_4(ans=ans, test=test, trial=trial, &
+          & term1=term1, term2=term2, dim=term2, c=c)
+    END IF
+!!
+  CASE (DEL_X_ALL)
+!!
+!! term1 = dt
+!! term2 = del_x_all
+!! c = vector
+!!
+    IF (rankC .EQ. VECTOR) THEN
+      CALL STCM_8(ans=ans, test=test, trial=trial, c=c, projectOn='trial')
+!!
+!! term1 = dt
+!! term2 = del_x_all
+!! c = scalar
+!!
+    ELSEIF (rankC .EQ. SCALAR) THEN
+      CALL STCM_5(ans=ans, test=test, trial=trial, &
+          & term1=term1, term2=term2, c=c)
+    END IF
+  END SELECT
+END SELECT
+
 END PROCEDURE Mat4_STConvectiveMatrix_1
-
-!----------------------------------------------------------------------------
-!                                                         STConvectiveMatrix
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE Mat4_STConvectiveMatrix_1b
-#include "./Mat4_STConvectiveMatrix_1.inc"
-END PROCEDURE Mat4_STConvectiveMatrix_1b
 
 !----------------------------------------------------------------------------
 !                                                           ConvectiveMatrix
@@ -42,209 +295,12 @@ END PROCEDURE Mat4_STConvectiveMatrix_1b
 
 MODULE PROCEDURE Mat2_STConvectiveMatrix_1
 REAL(DFP), ALLOCATABLE :: mat4(:, :, :, :)
-mat4 = STConvectiveMatrix(test=test, trial=trial, c=c, term1=term1, &
-     & term2=term2)
+mat4 = STConvectiveMatrix(test=test, trial=trial, term1=term1, &
+     & term2=term2, c=c, projectOn=projectOn)
 CALL convert(from=mat4, to=ans)
 IF (ALLOCATED(mat4)) DEALLOCATE (mat4)
 END PROCEDURE Mat2_STConvectiveMatrix_1
 
-!----------------------------------------------------------------------------
-!                                                           ConvectiveMatrix
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE Mat2_STConvectiveMatrix_1b
-REAL(DFP), ALLOCATABLE :: mat4(:, :, :, :)
-mat4 = STConvectiveMatrix(test=test, trial=trial, c=c, term1=term1, &
-     & term2=term2)
-CALL convert(from=mat4, to=ans)
-IF (ALLOCATED(mat4)) DEALLOCATE (mat4)
-END PROCEDURE Mat2_STConvectiveMatrix_1b
-
-!----------------------------------------------------------------------------
-!                                                         STConvectiveMatrix
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE Mat4_STConvectiveMatrix_2
-
-SELECT CASE (term1)
-
-CASE (DEL_NONE)
-
-  IF (dim .EQ. DEL_X_ALL) THEN
-    CALL case2_STConvectiveMatrix_2(ans=ans, test=test, trial=trial, &
-        & term1=term1, term2=term2, dim=dim, c=c)
-  ELSE
-    CALL case1_STConvectiveMatrix_2(ans=ans, test=test, trial=trial, &
-         & term1=term1, term2=term2, dim=dim, c=c)
-  END IF
-
-CASE (DEL_X, DEL_Y, DEL_Z)
-
-  select case (term2)
-  case (DEL_NONE)
-      CALL case1_STConvectiveMatrix_2(ans=ans, test=test, trial=trial, &
-           & term1=term1, term2=term2, dim=dim, c=c)
-  case (DEL_Dt)
-
-  end select
-
-CASE( DEL_X_ALL )
-
-CASE (DEL_Dt)
-
-END SELECT
-
-! IF (dim .GT. 0) THEN
-!   CALL case1_STConvectiveMatrix_2(ans=ans, test=test, trial=trial, &
-!        & term1=term1, term2=term2, dim=dim, c=c)
-! ELSE
-!   CALL case2_STConvectiveMatrix_2(ans=ans, test=test, trial=trial, &
-!        & term1=term1, term2=term2, dim=dim, c=c)
-! END IF
-END PROCEDURE Mat4_STConvectiveMatrix_2
-
-!----------------------------------------------------------------------------
-!                                                           ConvectiveMatrix
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE Mat2_STConvectiveMatrix_2
-REAL(DFP), ALLOCATABLE :: mat4(:, :, :, :)
-mat4 = STConvectiveMatrix(test=test, trial=trial, term1=term1, &
-     & term2=term2, dim=dim, c=c)
-CALL convert(from=mat4, to=ans)
-IF (ALLOCATED(mat4)) DEALLOCATE (mat4)
-END PROCEDURE Mat2_STConvectiveMatrix_2
-
-!----------------------------------------------------------------------------
-!                                                           ConvectiveMatrix
-!----------------------------------------------------------------------------
-
-PURE SUBROUTINE case1_STConvectiveMatrix_2(ans, test, trial, term1, &
-     & term2, dim, c)
-  REAL(DFP), ALLOCATABLE, INTENT(INOUT) :: ans(:, :, :, :)
-  CLASS(STElemshapeData_), INTENT(IN) :: test(:)
-  CLASS(STElemshapeData_), INTENT(IN) :: trial(:)
-  INTEGER(I4B), INTENT(IN) :: term1
-  INTEGER(I4B), INTENT(IN) :: term2
-  INTEGER(I4B), INTENT(IN) :: dim
-  TYPE(FEVariable_), OPTIONAL, INTENT(IN) :: c
-    !! Define internal variables
-  INTEGER(I4B) :: ipt, ips
-  REAL(DFP), ALLOCATABLE :: iajb(:, :, :, :)
-  REAL(DFP), ALLOCATABLE :: realval(:)
-  REAL(DFP), ALLOCATABLE :: cbar(:, :)
-    !! main
-    !! make c bar at ips and ipt
-  IF (PRESENT(c)) THEN
-    CALL GetInterpolation(obj=trial, val=c, interpol=cbar)
-  ELSE
-    CALL Reallocate(cbar, SIZE(trial(1)%N, 2), SIZE(trial))
-    cbar = 1.0_DFP
-  END IF
-    !!
-  CALL Reallocate(iajb, SIZE(test(1)%N, 1), SIZE(test(1)%T), &
-       & SIZE(trial(1)%N, 1), SIZE(trial(1)%T))
-    !!
-  IF (term1 .EQ. 0 .AND. term2 .EQ. 1) THEN
-    DO ipt = 1, SIZE(trial)
-      realval = trial(ipt)%js * trial(ipt)%ws * trial(ipt)%thickness * &
-           & trial(ipt)%wt * trial(ipt)%jt * cbar(:, ipt)
-      DO ips = 1, SIZE(realval)
-        iajb = iajb + realval(ips) &
-             & * outerprod( &
-             & outerprod(test(ipt)%N(:, ips), test(ipt)%T), &
-             & trial(ipt)%dNTdXt(:, :, dim, ips))
-      END DO
-    END DO
-  ELSE IF (term1 .EQ. 1 .AND. term2 .EQ. 0) THEN
-    DO ipt = 1, SIZE(trial)
-      realval = trial(ipt)%js * trial(ipt)%ws * trial(ipt)%thickness * &
-           & trial(ipt)%wt * trial(ipt)%jt * cbar(:, ipt)
-      DO ips = 1, SIZE(realval)
-        iajb = iajb + realval(ips) &
-             & * outerprod( &
-             & trial(ipt)%dNTdXt(:, :, dim, ips), &
-             & outerprod(trial(ipt)%N(:, ips), trial(ipt)%T))
-      END DO
-    END DO
-  END IF
-  CALL swap(a=ans, b=iajb, i1=1, i2=3, i3=2, i4=4)
-    !! cleanup
-  IF (ALLOCATED(iajb)) DEALLOCATE (iajb)
-  IF (ALLOCATED(realval)) DEALLOCATE (realval)
-  IF (ALLOCATED(cbar)) DEALLOCATE (cbar)
-END SUBROUTINE case1_STConvectiveMatrix_2
-
-!----------------------------------------------------------------------------
-!                                                           ConvectiveMatrix
-!----------------------------------------------------------------------------
-
-PURE SUBROUTINE case2_STConvectiveMatrix_2(ans, test, trial, term1, &
-    & term2, dim, c)
-  REAL(DFP), ALLOCATABLE, INTENT(INOUT) :: ans(:, :, :, :)
-  CLASS(STElemshapeData_), INTENT(IN) :: test(:)
-  CLASS(STElemshapeData_), INTENT(IN) :: trial(:)
-  INTEGER(I4B), INTENT(IN) :: term1
-  INTEGER(I4B), INTENT(IN) :: term2
-  INTEGER(I4B), INTENT(IN) :: dim
-  TYPE(FEVariable_), OPTIONAL, INTENT(IN) :: c
-    !! Define internal variables
-  INTEGER(I4B) :: nsd, ll, kk
-  REAL(DFP), ALLOCATABLE, DIMENSION(:, :, :, :) :: m1, m2, m3
-  !! main
-  nsd = trial(1)%refelem%nsd
-  SELECT CASE (nsd)
-  CASE (1)
-    CALL case1_STConvectiveMatrix_2(ans, test, trial, term1, term2, 1, c)
-  CASE (2)
-    CALL case1_STConvectiveMatrix_2(m1, test, trial, term1, term2, 1, c)
-    CALL case1_STConvectiveMatrix_2(m2, test, trial, term1, term2, 2, c)
-    IF (term1 .EQ. 0 .AND. term2 .EQ. 1) THEN
-      CALL Reallocate(ans, SIZE(m1, 1), nsd * SIZE(m1, 2), &
-           & SIZE(m1, 3), SIZE(m1, 4))
-      DO ll = 1, SIZE(ans, 4)
-        DO kk = 1, SIZE(ans, 3)
-         ans(:, :, kk, ll) = ColConcat(a=m1(:, :, kk, ll), b=m2(:, :, kk, ll))
-        END DO
-      END DO
-    ELSE IF (term1 .EQ. 1 .AND. term2 .EQ. 0) THEN
-      CALL Reallocate(ans, nsd * SIZE(m1, 1), SIZE(m1, 2), &
-           & SIZE(m1, 3), SIZE(m1, 4))
-      DO ll = 1, SIZE(ans, 4)
-        DO kk = 1, SIZE(ans, 3)
-         ans(:, :, kk, ll) = RowConcat(a=m1(:, :, kk, ll), b=m2(:, :, kk, ll))
-        END DO
-      END DO
-    END IF
-    DEALLOCATE (m1, m2)
-  CASE (3)
-    CALL case1_STConvectiveMatrix_2(m1, test, trial, term1, term2, 1, c)
-    CALL case1_STConvectiveMatrix_2(m2, test, trial, term1, term2, 2, c)
-    CALL case1_STConvectiveMatrix_2(m3, test, trial, term1, term2, 3, c)
-    IF (term1 .EQ. 0 .AND. term2 .EQ. 1) THEN
-      CALL Reallocate(ans, SIZE(m1, 1), nsd * SIZE(m1, 2), &
-           & SIZE(m1, 3), SIZE(m1, 4))
-      DO ll = 1, SIZE(ans, 4)
-        DO kk = 1, SIZE(ans, 3)
-          ans(:, :, kk, ll) = ColConcat( &
-               & a=ColConcat(a=m1(:, :, kk, ll), b=m2(:, :, kk, ll)), &
-               & b=m3(:, :, kk, ll))
-        END DO
-      END DO
-    ELSE IF (term1 .EQ. 1 .AND. term2 .EQ. 0) THEN
-      CALL Reallocate(ans, nsd * SIZE(m1, 1), SIZE(m1, 2), &
-           & SIZE(m1, 3), SIZE(m1, 4))
-      DO ll = 1, SIZE(ans, 4)
-        DO kk = 1, SIZE(ans, 3)
-          ans(:, :, kk, ll) = RowConcat( &
-               & a=RowConcat(a=m1(:, :, kk, ll), b=m2(:, :, kk, ll)), &
-               & b=m3(:, :, kk, ll))
-        END DO
-      END DO
-    END IF
-    DEALLOCATE (m1, m2, m3)
-  END SELECT
-END SUBROUTINE case2_STConvectiveMatrix_2
 !----------------------------------------------------------------------------
 !
 !----------------------------------------------------------------------------
