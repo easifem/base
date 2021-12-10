0e790dbf99e8eca133623577040a53156d7cec21
Vikas <vickysharma0812@gmail.com>
2021-12-10 21:36:19 +0530

    elemsd

--- src/submodules/ElemshapeData/src/ElemshapeData_Method@getMethod.F90
+++ src/submodules/ElemshapeData/src/ElemshapeData_Method@getMethod.F90
@@ -21,105 +21,6 @@ IMPLICIT NONE
 CONTAINS
 
 !----------------------------------------------------------------------------
-!                                                       getProjectionOfdNdXt
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE elemsd_GetProjectionOfdNdXt_1
-! Define internal variables
-INTEGER(I4B) :: i
-CALL Reallocate(cdNdXt, SIZE(obj%N, 1), SIZE(obj%N, 2))
-DO i = 1, SIZE(obj%N, 2)
-  cdNdXt(:, i) = MATMUL(obj%dNdXt(:, :, i), Val)
-END DO
-END PROCEDURE elemsd_GetProjectionOfdNdXt_1
-
-!----------------------------------------------------------------------------
-!                                                       getProjectionOfdNdXt
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE elemsd_GetProjectionOfdNdXt_2
-INTEGER(I4B) :: ii
-REAL(DFP), ALLOCATABLE :: CBar(:, :)
-!! main
-!! Compute CBar from Val
-SELECT CASE (Val%VarType)
-CASE (Constant)
-  CALL Reallocate(CBar, obj%refelem%nsd, SIZE(obj%N, 2))
-  CBar(:, 1) = Get(Val, TypeVariableVector, TypeVariableConstant)
-  DO ii = 2, SIZE(CBar, 2)
-    CBar(:, ii) = CBar(:, 1)
-  END DO
-CASE (Space)
-  IF (Val%DefineOn .EQ. Nodal) THEN
-    CALL getInterpolation(obj=obj, Interpol=CBar, &
-      & Val=Get(Val, TypeVariableVector, TypeVariableSpace))
-  ELSE
-    CBar = Get(Val, TypeVariableVector, TypeVariableSpace)
-  END IF
-CASE (SpaceTime)
-  SELECT TYPE (obj)
-  TYPE IS (STElemShapeData_)
-    CALL getInterpolation(obj=obj, Interpol=CBar, &
-      & Val=Get(Val, TypeVariableVector, TypeVariableSpaceTime))
-  END SELECT
-END SELECT
-CALL Reallocate(cdNdXt, SIZE(obj%N, 1), SIZE(obj%N, 2))
-DO ii = 1, SIZE(obj%N, 2)
-  cdNdXt(:, ii) = MATMUL(obj%dNdXt(:, :, ii), CBar(:, ii))
-END DO
-IF (ALLOCATED(CBar)) DEALLOCATE (CBar)
-END PROCEDURE elemsd_GetProjectionOfdNdXt_2
-
-!----------------------------------------------------------------------------
-!                                                      getProjectionOfdNTdXt
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE getProjectionOfdNTdXt_1
-INTEGER(I4B) :: ii
-CALL Reallocate(cdNTdXt, SIZE(obj%N, 1), SIZE(obj%T), SIZE(obj%N, 2))
-DO ii = 1, SIZE(obj%N, 2)
-  cdNTdXt(:, :, ii) = MATMUL(obj%dNTdXt(:, :, :, ii), Val)
-END DO
-END PROCEDURE getProjectionOfdNTdXt_1
-
-!----------------------------------------------------------------------------
-!                                                      getProjectionOfdNTdXt
-!----------------------------------------------------------------------------
-
-MODULE PROCEDURE getProjectionOfdNTdXt_2
-INTEGER(I4B) :: ii
-REAL(DFP), ALLOCATABLE :: CBar(:, :)
-!! main
-SELECT CASE (Val%VarType)
-!! constant variable type
-CASE (Constant)
-  CALL Reallocate(CBar, obj%refelem%nsd, SIZE(obj%N, 2))
-  CBar(:, 1) = Get(Val, TypeVariableVector, TypeVariableConstant)
-  DO ii = 2, SIZE(CBar, 2)
-    CBar(ii, :) = CBar(:, 1)
-  END DO
-!! spatial variable
-CASE (Space)
-  IF (Val%DefineOn .EQ. Nodal) THEN
-    CALL GetInterpolation(obj=obj, Interpol=CBar, &
-      & Val=Get(Val, TypeVariableVector, TypeVariableSpace))
-  ELSE
-    CBar = Get(Val, TypeVariableVector, TypeVariableSpace)
-  END IF
-!! space-time variable
-CASE (SpaceTime)
-  CALL getInterpolation(obj=obj, Interpol=CBar, &
-       & Val=Get(Val, TypeVariableVector, TypeVariableSpaceTime))
-END SELECT
-!!
-CALL Reallocate(cdNTdXt, SIZE(obj%N, 1), SIZE(obj%T), SIZE(obj%N, 2))
-DO ii = 1, SIZE(obj%N, 2)
-  cdNTdXt(:, :, ii) = MATMUL(obj%dNTdXt(:, :, :, ii), CBar(:, ii))
-END DO
-DEALLOCATE (CBar)
-END PROCEDURE getProjectionOfdNTdXt_2
-
-!----------------------------------------------------------------------------
 !                                                             getUnitNormal
 !----------------------------------------------------------------------------
 
@@ -174,37 +75,172 @@ END SUBROUTINE vector_getUnitNormal_3
 !----------------------------------------------------------------------------
 
 MODULE PROCEDURE elemsd_getSUPGParam
-INTEGER(I4B) :: ii
-REAL(DFP) :: t1, t2, t3
-REAL(DFP), ALLOCATABLE :: p(:, :), r(:, :), q(:, :), ans(:), nubar(:)
-type(FEVariable_) :: rvar
-!! main
-CALL GetProjectionOfdNdXt(obj=obj, cdNdXt=p, val=c)
-CALL GetUnitNormal(obj=obj, val=val, r=r)
-rvar = QuadratureVariable(r, TypeFEVariableVector, TypeFEVariableSpace)
-CALL GetProjectionOfdNdXt(obj=obj, cdNdXt=q, val=rvar)
-CALL GetInterpolation(obj=obj, val=nu, interpol=nubar)
-!!
-IF (dt .GT. zero) THEN
-  t2 = 2.0_DFP / dt
-ELSE
-  t2 = 0.0_DFP
-END IF
-!!
-CALL reallocate(ans, SIZE(obj%N, 2))
-DO ii = 1, SIZE(ans)
-  t1 = SUM(ABS(p(:, ii)))
-  t3 = nubar(ii) * (SUM(ABS(q(:, ii))))**2
-  ans(ii) = SQRT(1.0_DFP / (t1**2 + t2**2 + t3**2))
-END DO
-tau = QuadratureVariable(ans, TypeFEVariableScalar, TypeFEVariableSpace)
-!! cleanup
-IF(ALLOCATED(p)) DEALLOCATE(p)
-IF(ALLOCATED(r)) DEALLOCATE(r)
-IF(ALLOCATED(q)) DEALLOCATE(q)
-IF(ALLOCATED(ans)) DEALLOCATE(ans)
-IF(ALLOCATED(nubar)) DEALLOCATE(nubar)
-CALL Deallocate(rvar)
+SELECT TYPE (obj)
+TYPE is (ElemshapeData_)
+  CALL elemsd_getSUPGParam_a(obj=obj, tau=tau, c=c, val=val, nu=nu, &
+       & dt=dt, opt=opt)
+CLASS is (STElemshapeData_)
+  CALL elemsd_getSUPGParam_b(obj=obj, tau=tau, c=c, val=val, nu=nu, &
+       & dt=dt, opt=opt)
+END SELECT
 END PROCEDURE elemsd_getSUPGParam
+
+!----------------------------------------------------------------------------
+!                                                               getSUPGParam
+!----------------------------------------------------------------------------
+
+PURE SUBROUTINE elemsd_getSUPGParam_a(obj, tau, c, val, nu, dt, opt)
+  CLASS(ElemshapeData_), INTENT(IN) :: obj
+  !! element shape data
+  TYPE(FEVariable_), INTENT(INOUT) :: tau
+  !! stabilizing parameters
+  TYPE(FEVariable_), INTENT(IN) :: c
+  !! convective velocity
+  !! vector variable
+  TYPE(FEVariable_), INTENT(IN) :: val
+  !! solution
+  TYPE(FEVariable_), INTENT(IN) :: nu
+  !! diffusivity coefficient
+  REAL(DFP), OPTIONAL, INTENT(IN) :: dt
+  !! time step size
+  !! default value is zero
+  INTEGER(I4B), OPTIONAL, INTENT(IN) :: opt
+  !! define internal variables
+  !! futuristic option to implement other definitions
+  INTEGER(I4B) :: ii
+  REAL(DFP) :: t1, t2, t3
+  REAL(DFP), ALLOCATABLE :: p(:, :)
+  !! cdNdXt
+  REAL(DFP), ALLOCATABLE :: r(:, :)
+  !! unit normal
+  REAL(DFP), ALLOCATABLE :: q(:, :)
+  !! rdNdXt
+  REAL(DFP), ALLOCATABLE :: ans(:)
+  !! result
+  REAL(DFP), ALLOCATABLE :: nubar(:)
+  !! value of nu at space quadrature points
+  TYPE(FEVariable_) :: rvar
+  !! vector variable for keeping r
+  !!
+  !! MAIN
+  !!
+  CALL GetProjectionOfdNdXt(obj=obj, cdNdXt=p, val=c)
+  CALL GetUnitNormal(obj=obj, val=val, r=r)
+  rvar = QuadratureVariable(r, TypeFEVariableVector, TypeFEVariableSpace)
+  CALL GetProjectionOfdNdXt(obj=obj, cdNdXt=q, val=rvar)
+  CALL GetInterpolation(obj=obj, val=nu, interpol=nubar)
+  !!
+  IF (PRESENT(dt)) THEN
+    IF (dt .GT. zero) THEN
+      t2 = 2.0_DFP / dt
+    ELSE
+      t2 = 0.0_DFP
+    END IF
+  ELSE
+    t2 = 0.0_DFP
+  END IF
+  !!
+  CALL Reallocate(ans, SIZE(obj%N, 2))
+  DO ii = 1, SIZE(ans)
+    t1 = SUM(ABS(p(:, ii)))
+    t3 = nubar(ii) * (SUM(ABS(q(:, ii))))**2
+    ans(ii) = SQRT(1.0_DFP / (t1**2 + t2**2 + t3**2))
+  END DO
+  tau = QuadratureVariable(ans, TypeFEVariableScalar, TypeFEVariableSpace)
+  !! cleanup
+  IF (ALLOCATED(p)) DEALLOCATE (p)
+  IF (ALLOCATED(r)) DEALLOCATE (r)
+  IF (ALLOCATED(q)) DEALLOCATE (q)
+  IF (ALLOCATED(ans)) DEALLOCATE (ans)
+  IF (ALLOCATED(nubar)) DEALLOCATE (nubar)
+  CALL DEALLOCATE (rvar)
+END SUBROUTINE elemsd_getSUPGParam_a
+
+!----------------------------------------------------------------------------
+!                                                               getSUPGParam
+!----------------------------------------------------------------------------
+
+PURE SUBROUTINE elemsd_getSUPGParam_b(obj, tau, c, val, nu, dt, opt)
+  CLASS(STElemshapeData_), INTENT(IN) :: obj
+  !! space-time element shape data
+  TYPE(FEVariable_), INTENT(INOUT) :: tau
+  !! stabilization parameter
+  TYPE(FEVariable_), INTENT(IN) :: c
+  !! convective velocity
+  !! vector variable
+  TYPE(FEVariable_), INTENT(IN) :: val
+  !! solution
+  !! scalar/vector variable
+  TYPE(FEVariable_), INTENT(IN) :: nu
+  !! diffusivity
+  REAL(DFP), OPTIONAL, INTENT(IN) :: dt
+  !! time-step size
+  INTEGER(I4B), OPTIONAL, INTENT(IN) :: opt
+  !! futuristic
+  !!
+  !! INTERNAL VARIABLES
+  !!
+  INTEGER(I4B) :: ii, jj
+  REAL(DFP) :: t12, t3
+  REAL(DFP), ALLOCATABLE :: p(:, :, :)
+  !! cdNTdxt
+  REAL(DFP), ALLOCATABLE :: r(:, :)
+  !! vector at space quad points
+  REAL(DFP), ALLOCATABLE :: q(:, :, :)
+  !!
+  REAL(DFP), ALLOCATABLE :: ans(:)
+  REAL(DFP), ALLOCATABLE :: nubar(:)
+  TYPE(FEVariable_) :: rvar
+  !!
+  !! MAIN
+  !!
+  CALL GetProjectionOfdNTdXt(obj=obj, cdNTdXt=p, val=c)
+  !! make cdNTdxt + dNTdt
+  p = p + obj%dNTdt
+  CALL GetUnitNormal(obj=obj, val=val, r=r)
+  rvar = QuadratureVariable(r, TypeFEVariableVector, TypeFEVariableSpace)
+  CALL GetProjectionOfdNTdXt(obj=obj, cdNTdXt=q, val=rvar)
+  CALL GetInterpolation(obj=obj, val=nu, interpol=nubar)
+  !!
+  CALL reallocate(ans, SIZE(obj%N, 2))
+  DO ii = 1, SIZE(ans, 1)
+    t12 = SUM(ABS(p(:, :, ii)))
+    t3 = nubar(ii) * (SUM(ABS(q(:, :, ii))))**2
+    ans(ii) = SQRT(1.0_DFP / (t12**2 + t3**2))
+  END DO
+  tau = QuadratureVariable(ans, TypeFEVariableScalar, TypeFEVariableSpace)
+  !! cleanup
+  IF (ALLOCATED(p)) DEALLOCATE (p)
+  IF (ALLOCATED(r)) DEALLOCATE (r)
+  IF (ALLOCATED(q)) DEALLOCATE (q)
+  IF (ALLOCATED(ans)) DEALLOCATE (ans)
+  IF (ALLOCATED(nubar)) DEALLOCATE (nubar)
+  CALL DEALLOCATE (rvar)
+END SUBROUTINE elemsd_getSUPGParam_b
+
+!----------------------------------------------------------------------------
+!                                                             GetSUPGParam
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE stelemsd_GetSUPGParam
+INTEGER(I4B) :: ii
+REAL(DFP), ALLOCATABLE :: ans(:, :)
+TYPE(FEVariable_) :: a
+!! main
+CALL Reallocate(ans, SIZE(obj(1)%N, 2), SIZE(obj))
+DO ii = 1, SIZE(obj)
+  CALL GetSUPGParam(obj=obj(ii), tau=a, c=c, val=val, nu=nu, dt=dt, opt=opt)
+  ans(:, ii) = Get(a, TypeFEVariableScalar, TypeFEVariableSpace)
+END DO
+!!
+tau = QuadratureVariable(ans, TypeFEVariableScalar, TypeFEVariableSpaceTime)
+!!
+CALL DEALLOCATE (a)
+DEALLOCATE (ans)
+END PROCEDURE stelemsd_GetSUPGParam
+
+!----------------------------------------------------------------------------
+!
+!----------------------------------------------------------------------------
 
 END SUBMODULE getMethod
