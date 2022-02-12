b759999c75ef3de5893d585198b8afd6db1411c0
Vikas Sharma <vickysharma0812@gmail.com>
2022-02-12 19:03:38 +0530

    Create CSRMatrix_Method@LUSolveMethods.F90

---
+++ src/submodules/CSRMatrix/src/CSRMatrix_Method@LUSolveMethods.F90
@@ -0,0 +1,43 @@
+! This program is a part of EASIFEM library
+! Copyright (C) 2020-2021  Vikas Sharma, Ph.D
+!
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <https: //www.gnu.org/licenses/>
+!
+
+!> authors: Vikas Sharma, Ph. D.
+! date: 14 July 2021
+! summary: This submodule contains methods fors solving LU x = y
+
+SUBMODULE(CSRMatrix_Method) LUSolveMethods
+USE BaseMethod
+IMPLICIT NONE
+CONTAINS
+
+!----------------------------------------------------------------------------
+!                                                                 LUSOLVE
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE csrMat_LUSOLVE
+  CALL LUSOL(SIZE(rhs), rhs, sol, alu, jlu, ju)
+END PROCEDURE csrMat_LUSOLVE
+
+!----------------------------------------------------------------------------
+!                                                                 LUTSOLVE
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE csrMat_LUTSOLVE
+  CALL LUTSOL(SIZE(rhs), rhs, sol, alu, jlu, ju)
+END PROCEDURE csrMat_LUTSOLVE
+
+END SUBMODULE LUSolveMethods
