610a8b508207221c0d1aace2aea9f5074a5e8f68
Vikas Sharma <vickysharma0812@gmail.com>
2022-02-15 08:24:23 +0530

    Update DOF_Method@setMethods.F90

--- src/submodules/DOF/src/DOF_Method@setMethods.F90
+++ src/submodules/DOF/src/DOF_Method@setMethods.F90
@@ -16,269 +16,337 @@
 !
 
 SUBMODULE(DOF_Method) setMethods
-  !! This submodule defines the methods for setting/changing values in
-  !! fortran real vector using [[dof_]] object
-
 IMPLICIT NONE
 
 CONTAINS
 
 !----------------------------------------------------------------------------
-!                                                                    setValue
+!                                                                    setvalue
 !----------------------------------------------------------------------------
 
 MODULE PROCEDURE dof_set1
   INTEGER( I4B ) :: tdof, idof, i, n, m
-
+  !!
   tdof = .tdof. obj
-  n = SIZE( Nptrs ); m = SIZE( Val )
-
-  ASSOCIATE( vm => obj%ValMap )
+  n = SIZE( nodenum )
+  m = SIZE( value )
+  !!
+  ASSOCIATE( vm => obj%valmap )
     SELECT CASE( obj% StorageFMT )
-    CASE( dof_FMT )
+    !!
+    !!
+    !!
+    !!
+    CASE( DOF_FMT )
+      !!
       IF( m .NE. n ) THEN
+        !!
         IF( m .EQ. 1 ) THEN
+          !!
           DO idof = 1, tdof
-            Vec( vm( idof ) - 1 + Nptrs ) = Val( 1 )
+            Vec( vm( idof ) - 1 + nodenum ) = value( 1 )
           END DO
+          !!
         ELSE IF( m .EQ. tdof * n ) THEN
+          !!
           IF( Conversion( 1 ) .EQ. nodesToDOF ) THEN
+            !!
             DO idof = 1, tdof
               DO i = 1, n
-                Vec( vm(idof)-1+Nptrs( i ) ) = Val( ( i - 1 ) * tdof + idof  )
+                Vec( vm(idof)-1+nodenum( i ) ) = &
+                  & value( ( i - 1 ) * tdof + idof )
               END DO
             END DO
+            !!
           ELSE
+            !!
             DO idof = 1, tdof
-              Vec( vm(idof)-1+Nptrs) = Val( ( idof - 1 ) * n + 1 : idof * n )
+              Vec( vm(idof)-1+nodenum) = &
+                & value( ( idof - 1 ) * n + 1 : idof * n )
             END DO
+            !!
           END IF
+          !!
         END IF
+        !!
       ELSE
+        !!
         DO idof = 1, tdof
-          Vec( vm( idof ) - 1 + Nptrs ) = Val( : )
+          Vec( vm( idof ) - 1 + nodenum ) = value( : )
         END DO
+        !!
       END IF
-    !
-    CASE( Nodes_FMT )
+    !!
+    !!
+    !!
+    !!
+    CASE( NODES_FMT )
+      !!
       IF( m .NE. n ) THEN
+        !!
         IF( m .EQ. 1 ) THEN
+          !!
           DO idof = 1, tdof
-            Vec( (Nptrs - 1 ) * tdof + idof ) = Val( 1 )
+            Vec( (nodenum - 1 ) * tdof + idof ) = value( 1 )
           END DO
+          !!
         ELSE IF( m .EQ. tdof * n ) THEN
+          !!
           IF( Conversion( 1 ) .EQ. DOFToNodes ) THEN
+            !!
             DO idof = 1, tdof
               DO i = 1, n
-                Vec( ( Nptrs( i ) - 1 ) * tdof + idof ) &
-                  & = Val( ( idof - 1 ) * n + i  )
+                Vec( ( nodenum( i ) - 1 ) * tdof + idof ) &
+                  & = value( ( idof - 1 ) * n + i  )
               END DO
             END DO
+            !!
           ELSE
+            !!
             DO idof = 1, tdof
               DO i = 1, n
-                Vec( ( Nptrs( i ) - 1 ) * tdof + idof ) &
-                  & = Val( ( i - 1 ) * tdof + idof )
+                Vec( ( nodenum( i ) - 1 ) * tdof + idof ) &
+                  & = value( ( i - 1 ) * tdof + idof )
               END DO
             END DO
+            !!
           END IF
+          !!
         END IF
+        !!
       ELSE
+        !!
         DO idof = 1, tdof
-          Vec( (Nptrs - 1) * tdof + idof ) = Val( : )
+          Vec( (nodenum - 1) * tdof + idof ) = value( : )
         END DO
+        !!
       END IF
+      !!
     END SELECT
+    !!
+    !!
+    !!
+    !!
   END ASSOCIATE
+  !!
 END PROCEDURE dof_set1
 
 !----------------------------------------------------------------------------
-!                                                                 setValue
+!                                                                    setvalue
 !----------------------------------------------------------------------------
 
 MODULE PROCEDURE dof_set2
-  INTEGER( I4B ) :: n, m, tdof
-  tdof = obj%Map( SIZE( obj%Map, 1 ), 4 ); n = SIZE(Nptrs); m=SIZE(Val)
-  ASSOCIATE( vm => obj%ValMap )
-    SELECT CASE( obj%StorageFMT )
-    CASE( dof_FMT )
-      IF( m .EQ. n ) THEN
-        Vec( vm( dofno ) - 1 + Nptrs ) = Val( : )
-      ELSE
-        Vec( vm( dofno ) - 1 + Nptrs ) = Val( 1 )
-      END IF
-    CASE( Nodes_FMT )
-      IF( m .EQ. n ) THEN
-        Vec( ( Nptrs - 1 ) * tdof + dofno ) = Val( : )
-      ELSE
-        Vec( ( Nptrs - 1 ) * tdof + dofno ) = Val( 1 )
-      END IF
-    END SELECT
-  END ASSOCIATE
+  vec( getIndex(obj=obj, nodenum=nodenum) ) = value
 END PROCEDURE dof_set2
 
 !----------------------------------------------------------------------------
-!                                                                 set
+!                                                                 setvalue
 !----------------------------------------------------------------------------
 
 MODULE PROCEDURE dof_set3
-  CALL set( Vec%val, obj, Nptrs, Val, Conversion )
+  !!
+  IF( SIZE(nodenum) .EQ. SIZE(value) ) THEN
+    Vec( &
+      & getNodeLoc( &
+      & obj=obj, &
+      & nodenum=nodenum, &
+      & idof=idof) ) = value( : )
+  ELSE
+    Vec( &
+      & getNodeLoc( &
+      & obj=obj, &
+      & nodenum=nodenum, &
+      & idof=idof) ) = value( 1 )
+  END IF
+  !!
 END PROCEDURE dof_set3
+
+!----------------------------------------------------------------------------
+!                                                                 setvalue
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE dof_set4
+  !!
+  IF( SIZE(nodenum) .EQ. SIZE(value) ) THEN
+    Vec( &
+      & getIndex( &
+      & obj=obj, &
+      & nodenum=nodenum, &
+      & ivar=ivar, &
+      & idof=idof) ) &
+      & = value( : )
+  ELSE
+    Vec( &
+      & getIndex( &
+      & obj=obj, &
+      & nodenum=nodenum, &
+      & ivar=ivar, &
+      & idof=idof) ) &
+      & = value( 1 )
+  END IF
+  !!
+END PROCEDURE dof_set4
 
 !----------------------------------------------------------------------------
 !                                                                 set
 !----------------------------------------------------------------------------
 
-MODULE PROCEDURE dof_set4
-  CALL set( Vec%val, obj, Nptrs, Val, dofno )
-END PROCEDURE dof_set4
+MODULE PROCEDURE dof_set5
+  !!
+  IF( SIZE(nodenum) .EQ. SIZE(value) ) THEN
+    !!
+    Vec( getNodeLoc(&
+      & obj=obj, &
+      & nodenum=nodenum, &
+      & ivar=ivar, &
+      & spacecompo=spacecompo, &
+      & timecompo=timecompo ) ) &
+      & = value( : )
+    !!
+  ELSE
+    !!
+    Vec( &
+      & getNodeLoc( &
+      & obj=obj, &
+      & nodenum=nodenum, &
+      & ivar=ivar, &
+      & spacecompo=spacecompo, &
+      & timecompo=timecompo ) ) &
+      & = value( 1 )
+    !!
+  END IF
+  !!
+END PROCEDURE dof_set5
 
 !----------------------------------------------------------------------------
-!                                                            AddContribution
+!                                                                 set
 !----------------------------------------------------------------------------
 
-MODULE PROCEDURE dof_add1
-  INTEGER( I4B ) :: tdof, idof, i, n, m
-
-  tdof = .tdof. obj
-  n = SIZE( Nptrs ); m = SIZE( Val )
-
-  ASSOCIATE( vm => obj%ValMap )
-    SELECT CASE( obj% StorageFMT )
-    CASE( dof_FMT )
-      IF( m .NE. n ) THEN
-        ! Vec( Nptrs ) += scale * Val( 1 )
-        IF( m .EQ. 1 ) THEN
-          DO idof = 1, tdof
-            Vec( vm( idof ) - 1 + Nptrs ) &
-              & = Vec( vm( idof ) - 1 + Nptrs ) &
-              & + scale * Val( 1 )
-          END DO
-
-        ! Vec_dof_i( Nptrs ) += scale * Val_dof_i( : )
-        ELSE IF( m .EQ. tdof * n ) THEN
-
-          IF( Conversion( 1 ) .EQ. nodesToDOF ) THEN
-
-            DO idof = 1, tdof
-              DO i = 1, n
-                Vec( vm(idof)-1+Nptrs( i ) ) &
-                  & = Vec( vm(idof)-1+Nptrs( i ) ) &
-                  & + scale * Val( ( i - 1 ) * tdof +idof)
-              END DO
-            END DO
-
-          ELSE
-
-            DO idof = 1, tdof
-              Vec( vm(idof)-1+Nptrs) &
-                & = Vec( vm(idof)-1+Nptrs) &
-                & + scale * Val( ( idof - 1 ) * n + 1 : idof * n )
-            END DO
-
-          END IF
-
-        END IF
-
-      ! Vec_dof_i(Nptrs) += scale * Val
-      ELSE
-
-        DO idof = 1, tdof
-          Vec( vm( idof ) - 1 + Nptrs ) &
-            & = Vec( vm( idof ) - 1 + Nptrs ) &
-            & + scale * Val( : )
-        END DO
-
-      END IF
-    !
-    CASE( Nodes_FMT )
-
-      IF( m .NE. n ) THEN
-
-        IF( m .EQ. 1 ) THEN
-
-          DO idof = 1, tdof
-            Vec( (Nptrs - 1 ) * tdof + idof ) &
-              & = Vec( (Nptrs - 1 ) * tdof + idof ) &
-              & + scale * Val( 1 )
-          END DO
-
-        ELSE IF( m .EQ. tdof * n ) THEN
-
-          IF( Conversion( 1 ) .EQ. DOFToNodes ) THEN
-
-            DO idof = 1, tdof
-              DO i = 1, n
-                Vec( ( Nptrs( i ) - 1 ) * tdof + idof ) &
-                  & = Vec( ( Nptrs( i ) - 1 ) * tdof + idof ) &
-                  & + scale * Val( ( idof - 1 ) * n + i  )
-              END DO
-            END DO
-
-          ELSE
-
-            DO idof = 1, tdof
-              DO i = 1, n
-                Vec( ( Nptrs( i ) - 1 ) * tdof + idof ) &
-                  & = Vec( ( Nptrs( i ) - 1 ) * tdof + idof ) &
-                  & + scale * Val( ( i - 1 ) * tdof + idof )
-              END DO
-            END DO
-
-          END IF
-        END IF
-
-      ELSE
-
-        DO idof = 1, tdof
-          Vec( (Nptrs - 1) * tdof + idof ) &
-            & = Vec( (Nptrs - 1) * tdof + idof ) &
-            & + scale * Val( : )
-        END DO
-
-      END IF
-
-    END SELECT
-  END ASSOCIATE
-END PROCEDURE dof_add1
+MODULE PROCEDURE dof_set6
+  !!
+  IF( SIZE(nodenum) .EQ. SIZE(value) * SIZE(timecompo) ) THEN
+    Vec( &
+      & getNodeLoc( &
+      & obj=obj, &
+      & nodenum=nodenum, &
+      & ivar=ivar, &
+      & spacecompo=spacecompo, &
+      & timecompo=timecompo ) ) &
+      & = value( : )
+  ELSE
+    Vec( &
+      & getNodeLoc( &
+      & obj=obj, &
+      & nodenum=nodenum, &
+      & ivar=ivar, &
+      & spacecompo=spacecompo, &
+      & timecompo=timecompo ) ) &
+      & = value( 1 )
+  END IF
+  !!
+END PROCEDURE dof_set6
 
 !----------------------------------------------------------------------------
-!                                                            AddContribution
+!                                                                 set
 !----------------------------------------------------------------------------
 
-MODULE PROCEDURE dof_add2
-  INTEGER( I4B ) :: n, m, tdof
+MODULE PROCEDURE dof_set7
+  !!
+  IF( SIZE(nodenum) .EQ. SIZE(value)*SIZE(spacecompo) ) THEN
+    Vec( getNodeLoc( &
+      & obj=obj, &
+      & nodenum=nodenum, &
+      & ivar=ivar, &
+      & spacecompo=spacecompo, &
+      & timecompo=timecompo ) ) &
+      & = value( : )
+  ELSE
+    Vec( &
+      & getNodeLoc( &
+      & obj=obj, &
+      & nodenum=nodenum, &
+      & ivar=ivar, &
+      & spacecompo=spacecompo, &
+      & timecompo=timecompo ) ) &
+      & = value( 1 )
+  END IF
+  !!
+END PROCEDURE dof_set7
 
-  tdof = obj%Map( SIZE( obj%Map, 1 ), 4 ); n = SIZE(Nptrs); m=SIZE(Val)
+!----------------------------------------------------------------------------
+!                                                                    setvalue
+!----------------------------------------------------------------------------
 
-  ASSOCIATE( vm => obj%ValMap )
-    SELECT CASE( obj%StorageFMT )
+MODULE PROCEDURE dof_set8
+  vec( &
+    & getIndex( &
+    & obj=obj, &
+    & nodenum=nodenum) ) = value
+END PROCEDURE dof_set8
 
-    CASE( dof_FMT )
+!----------------------------------------------------------------------------
+!                                                                 setvalue
+!----------------------------------------------------------------------------
 
-      IF( m .EQ. n ) THEN
-        Vec( vm( dofno ) - 1 + Nptrs ) &
-          & = Vec( vm( dofno ) - 1 + Nptrs ) &
-          & + scale * Val( : )
-      ELSE
-        Vec( vm( dofno ) - 1 + Nptrs ) &
-          & = Vec( vm( dofno ) - 1 + Nptrs ) &
-          & + scale * Val( 1 )
-      END IF
+MODULE PROCEDURE dof_set9
+  Vec( &
+    & getNodeLoc( &
+    & obj=obj, &
+    & nodenum=nodenum, &
+    & idof=idof) ) = value
+END PROCEDURE dof_set9
 
-    CASE( Nodes_FMT )
+!----------------------------------------------------------------------------
+!                                                                 setvalue
+!----------------------------------------------------------------------------
 
-      IF( m .EQ. n ) THEN
-        Vec( ( Nptrs - 1 ) * tdof + dofno ) &
-          & = Vec( ( Nptrs - 1 ) * tdof + dofno ) &
-          & + scale * Val( : )
-      ELSE
-        Vec( ( Nptrs - 1 ) * tdof + dofno ) &
-          & = Vec( ( Nptrs - 1 ) * tdof + dofno ) &
-          & + scale * Val( 1 )
-      END IF
-    END SELECT
-  END ASSOCIATE
-END PROCEDURE dof_add2
+MODULE PROCEDURE dof_set10
+  Vec( &
+    & getNodeLoc( &
+    & obj=obj, &
+    & nodenum=nodenum, &
+    & ivar=ivar, &
+    & idof=idof) ) = value
+END PROCEDURE dof_set10
+
+!----------------------------------------------------------------------------
+!                                                                 setvalue
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE dof_set11
+  Vec( getNodeLoc(obj=obj, &
+    & nodenum=nodenum, &
+    & ivar=ivar, &
+    & spacecompo=spacecompo, &
+    & timecompo=timecompo) ) = value
+END PROCEDURE dof_set11
+
+!----------------------------------------------------------------------------
+!                                                                 setvalue
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE dof_set12
+  Vec( getNodeLoc(obj=obj, &
+    & nodenum=nodenum, &
+    & ivar=ivar, &
+    & spacecompo=spacecompo, &
+    & timecompo=timecompo) ) = value
+END PROCEDURE dof_set12
+
+!----------------------------------------------------------------------------
+!                                                                 setvalue
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE dof_set13
+  Vec( getNodeLoc(obj=obj, &
+    & nodenum=nodenum, &
+    & ivar=ivar, &
+    & spacecompo=spacecompo, &
+    & timecompo=timecompo) ) = value
+END PROCEDURE dof_set13
+
+!----------------------------------------------------------------------------
+!
+!----------------------------------------------------------------------------
 
 END SUBMODULE setMethods
