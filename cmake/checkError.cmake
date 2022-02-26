0972e0664aca8f2153e9fb0e02781bc3da08627c
Vikas Sharma <vickysharma0812@gmail.com>
2022-02-26 20:53:26 +0530

    Update checkError.cmake

--- cmake/checkError.cmake
+++ cmake/checkError.cmake
@@ -1,3 +1,19 @@
+# This program is a part of EASIFEM library
+# Copyright (C) 2020-2021  Vikas Sharma, Ph.D
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https: //www.gnu.org/licenses/>
+#
 # SYSTEM INFORMATION
 # Disable in-source builds to prevent source tree corruption.
 IF(" ${CMAKE_CURRENT_SOURCE_DIR}" STREQUAL " ${CMAKE_CURRENT_BINARY_DIR}")
