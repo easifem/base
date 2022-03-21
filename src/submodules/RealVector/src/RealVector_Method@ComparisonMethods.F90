eed7cfa9349dfe33f4acca899a9733ba6ae84138
Vikas Sharma <vickysharma0812@gmail.com>
2022-03-21 09:58:26 +0530

    Create RealVector_Method@ComparisonMethods.F90

---
+++ src/submodules/RealVector/src/RealVector_Method@ComparisonMethods.F90
@@ -0,0 +1,51 @@
+! This program is a part of EASIFEM library
+! Copyright (C) 2020-2021  Vikas Sharma, Ph.D
+!
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <https: //www.gnu.org/licenses/>
+!
+
+SUBMODULE(RealVector_Method) OperatorMethods
+USE BaseMethod
+IMPLICIT NONE
+
+CONTAINS
+
+!----------------------------------------------------------------------------
+!                                                                 isEqual
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE realvec_isequal
+  IF( .NOT. ALLOCATED( obj%val ) ) THEN
+    ans = .FALSE.
+    return
+  END IF
+  !!
+  IF( .NOT. ALLOCATED( obj2%val ) ) THEN
+    ans = .FALSE.
+    return
+  END IF
+  !!
+  IF( SIZE( obj%val ) .NE. SIZE( obj2%val ) ) THEN
+    ans = .FALSE.
+    return
+  END IF
+  !!
+  IF( ALL( obj%val .APPROXEQ. obj2%val ) ) THEN
+    ans = .TRUE.
+  ELSE
+    ans = .FALSE.
+  END IF
+END PROCEDURE realvec_isequal
+
+END SUBMODULE OperatorMethods
