84aceb2807996ffe0140e155d53cfec178bb83ca
Vikas Sharma <Vickysharma0812@gmail.com>
2022-07-23 15:44:06 +0900

    BLAS1V modified

--- src/modules/BLAS/src/BLAS1V_Method.F90
+++ src/modules/BLAS/src/BLAS1V_Method.F90
@@ -17,7 +17,8 @@
 
 !> authors: Vikas Sharma, Ph. D.
 ! date: 7 March 2021
-! summary: This module contains BLAS1 methods for [[RealVector_]] and [[RealMatrix_]] data type.
+! summary: This module contains BLAS1 methods for [[RealVector_]] and
+! [[RealMatrix_]] data type.
 !
 !{!pages/BLAS.md}
 
@@ -38,7 +39,8 @@ PRIVATE
 !
 ! This function computes the absolute sum of a vector.
 !
-! $$\left| \left| V\right|  \right|_{1}  =\sum^{N}_{i=1} \left( \  \left| V_{i}\right|  \right)$$
+! $$\left| \left| V\right|  \right|_{1}  =\sum^{N}_{i=1} \left( \
+! \left| V_{i}\right|  \right)$$
 !
 !@note
 ! 	This function calls BLAS function ASUM.
@@ -72,7 +74,8 @@ END INTERFACE
 !
 ! This function computes the absolute sum of a vector.
 !
-! $$\left| \left| V\right|  \right|_{1}  =\sum^{N}_{i=1} \left( \  \left| V_{i}\right|  \right)$$
+! $$\left| \left| V\right|  \right|_{1}  =\sum^{N}_{i=1} \left( \
+! \left| V_{i}\right|  \right)$$
 !
 !@note
 ! 	This function calls [[ASUMScalar]] method
@@ -106,15 +109,15 @@ END INTERFACE
 !### Usage
 !
 !```fortran
-  ! integer( i4b ), parameter :: n = 100, m=5
-  ! integer( i4b ) :: i
-  ! type( RealVector_ ) :: obj( m )
-  ! real( dfp ) :: ans_l(m), ans
-  ! do i = 1, m
-  !   obj( i ) = RealVector(arange(1,n,1))
-  ! end do
-  ! ans = ASUM(obj)
-  ! call display( ans - (m*sum(obj(1)%val)), "test3: 0 if correct : " )
+! integer( i4b ), parameter :: n = 100, m=5
+! integer( i4b ) :: i
+! type( RealVector_ ) :: obj( m )
+! real( dfp ) :: ans_l(m), ans
+! do i = 1, m
+!   obj( i ) = RealVector(arange(1,n,1))
+! end do
+! ans = ASUM(obj)
+! call display( ans - (m*sum(obj(1)%val)), "test3: 0 if correct : " )
 !```
 
 INTERFACE
@@ -147,7 +150,9 @@ PUBLIC :: ASUM
 !
 !@note
 ! In joined state this subroutine creates new threads and share the work.
-! If this subroutine is called within parallel block (i.e., forked state) then it does not create any new threads. Each thread will call this subroutine while X, Y, A treated as shared type.
+! If this subroutine is called within parallel block (i.e., forked state)
+! then it does not create any new threads. Each thread will call this
+! subroutine while X, Y, A treated as shared type.
 !@endnote
 !
 !### Usage
@@ -191,14 +196,11 @@ END INTERFACE
 !
 !@note
 ! In joined state this subroutine creates new threads and share the work.
-! If this subroutine is called within parallel block (i.e., forked state) then it does not create any new threads. Each thread will call this subroutine while X, Y, A treated as shared type.
+! If this subroutine is called within parallel block (i.e., forked state)
+! then it does not create any new threads. Each thread will call this
+! subroutine while X, Y, A treated as shared type.
 !@endnote
 !
-!### Usage
-!
-!```fortran
-!
-!```
 
 INTERFACE
 MODULE SUBROUTINE scalarAXPYintrinsic( X, Y, A )
@@ -225,29 +227,31 @@ END INTERFACE
 !
 !@note
 ! In joined state this subroutine creates new threads and share the work.
-! If this subroutine is called within parallel block (i.e., forked state) then it does not create any new threads. Each thread will call this subroutine while X, Y, A treated as shared type.
+! If this subroutine is called within parallel block (i.e., forked state)
+! then it does not create any new threads. Each thread will call this
+! subroutine while X, Y, A treated as shared type.
 !@endnote
 !
 !### Usage
 !
 !```fortran
-  ! integer( i4b ), parameter :: n = 100, m = 4
-  ! integer( i4b ) :: i, tsize(m)
-  ! real( dfp ) :: a( m ), ans
-  ! type( RealVector_ ), allocatable :: x( : ), y( : ), z( : )
-  ! tsize = m; a = 1.0
-  ! call random_number( x, tsize )
-  ! call random_number( y, tsize )
-  ! call initiate( z, tsize )
-  ! do i = 1, m
-  !   z(i)%val = y(i)%val + a( i ) * x(i)%val
-  ! end do
-  ! call axpy( x = x, y = y, A = a )
-  ! ans = 0.0
-  ! do i = 1, m
-  !   ans = ans + asum( y(i)%val - z(i)%val )
-  ! end do
-  ! call display( ans, "test5: 0 if correct : " )
+! integer( i4b ), parameter :: n = 100, m = 4
+! integer( i4b ) :: i, tsize(m)
+! real( dfp ) :: a( m ), ans
+! type( RealVector_ ), allocatable :: x( : ), y( : ), z( : )
+! tsize = m; a = 1.0
+! call random_number( x, tsize )
+! call random_number( y, tsize )
+! call initiate( z, tsize )
+! do i = 1, m
+!   z(i)%val = y(i)%val + a( i ) * x(i)%val
+! end do
+! call axpy( x = x, y = y, A = a )
+! ans = 0.0
+! do i = 1, m
+!   ans = ans + asum( y(i)%val - z(i)%val )
+! end do
+! call display( ans, "test5: 0 if correct : " )
 !```
 
 INTERFACE
@@ -274,9 +278,11 @@ PUBLIC :: AXPY
 ! summary: 	This routine copies one vector into another
 !
 !# Introduction
-! This subroutine copies one [[RealVector_]] object into another object, i.e. `Y=X`. See figure given below:
+! This subroutine copies one [[RealVector_]] object into another object, i.e.
+! `Y=X`. See figure given below:
 !
-! <img src=|media|/scalar_copy_scalar.jpg alt="drawing" style="max-width:500px;"/>
+! <img src=|media|/scalar_copy_scalar.jpg alt="drawing"
+! style="max-width:500px;"/>
 !
 !@note
 ! 	This subroutine internally uses [[intrinsicCOPYintrinsic]] routine.
@@ -285,16 +291,16 @@ PUBLIC :: AXPY
 !### Usage
 !
 !```fortran
-  ! integer( i4b ), parameter :: n = 10000
-  ! type( RealVector_ ) :: x, y
-  ! real( dfp ), allocatable :: z( : )
-  ! call random_number( x, n )
-  ! call copy( x = x, y = y )
-  ! call display( asum( x%val - y%val ), "test6: 0 if correct : " )
-  ! call copy( y=z, x=x )
-  ! call display( asum( z - x%val ), "test6: 0 if correct : " )
-  ! call copy( y=x, x=z )
-  ! call display( asum( z - x%val ), "test6: 0 if correct : " )
+! integer( i4b ), parameter :: n = 10000
+! type( RealVector_ ) :: x, y
+! real( dfp ), allocatable :: z( : )
+! call random_number( x, n )
+! call copy( x = x, y = y )
+! call display( asum( x%val - y%val ), "test6: 0 if correct : " )
+! call copy( y=z, x=x )
+! call display( asum( z - x%val ), "test6: 0 if correct : " )
+! call copy( y=x, x=z )
+! call display( asum( z - x%val ), "test6: 0 if correct : " )
 !```
 
 INTERFACE
@@ -303,6 +309,12 @@ MODULE SUBROUTINE scalarCOPYscalar( Y, X )
   CLASS( RealVector_ ), INTENT( IN ) :: X
 END SUBROUTINE scalarCOPYscalar
 END INTERFACE
+
+INTERFACE COPY
+  MODULE PROCEDURE scalarCOPYscalar
+END INTERFACE COPY
+
+PUBLIC :: COPY
 
 !----------------------------------------------------------------------------
 !                                                                 COPY@BLAS1
@@ -322,24 +334,32 @@ END INTERFACE
 !### Usage
 !
 !```fortran
-  ! integer( i4b ), parameter :: n = 10000
-  ! type( RealVector_ ) :: x, y
-  ! real( dfp ), allocatable :: z( : )
-  ! call random_number( x, n )
-  ! call copy( x = x, y = y )
-  ! call display( asum( x%val - y%val ), "test6: 0 if correct : " )
-  ! call copy( y=z, x=x )
-  ! call display( asum( z - x%val ), "test6: 0 if correct : " )
-  ! call copy( y=x, x=z )
-  ! call display( asum( z - x%val ), "test6: 0 if correct : " )
+! integer( i4b ), parameter :: n = 10000
+! type( RealVector_ ) :: x, y
+! real( dfp ), allocatable :: z( : )
+! call random_number( x, n )
+! call copy( x = x, y = y )
+! call display( asum( x%val - y%val ), "test6: 0 if correct : " )
+! call copy( y=z, x=x )
+! call display( asum( z - x%val ), "test6: 0 if correct : " )
+! call copy( y=x, x=z )
+! call display( asum( z - x%val ), "test6: 0 if correct : " )
 !```
 
 INTERFACE
-MODULE SUBROUTINE scalarCOPYintrinsic( Y, X )
+MODULE SUBROUTINE scalarCOPYintrinsic_1a( Y, X )
   CLASS( RealVector_ ), INTENT( INOUT ) :: Y
-  REAL( DFP ), INTENT( IN ) :: X( : )
-END SUBROUTINE scalarCOPYintrinsic
+  REAL( Real32 ), INTENT( IN ) :: X( : )
+END SUBROUTINE scalarCOPYintrinsic_1a
+MODULE SUBROUTINE scalarCOPYintrinsic_1b( Y, X )
+  CLASS( RealVector_ ), INTENT( INOUT ) :: Y
+  REAL( Real64 ), INTENT( IN ) :: X( : )
+END SUBROUTINE scalarCOPYintrinsic_1b
 END INTERFACE
+
+INTERFACE COPY
+  MODULE PROCEDURE scalarCOPYintrinsic_1a, scalarCOPYintrinsic_1b
+END INTERFACE COPY
 
 !----------------------------------------------------------------------------
 !                                                                 COPY@BLAS1
@@ -350,34 +370,44 @@ END INTERFACE
 ! summary: 	This routine copies one vector into another
 !
 !# Introduction
-! This subroutine copy an instance of [[RealVector_]] in another fortran vector, i.e. `Val=obj`
+! This subroutine copy an instance of [[RealVector_]] in another fortran
+! vector, i.e. `Val=obj`
 !
 !@note
-! 	This subroutine internally calls [[intrinsicCOPYintrinsic]]. Also `Val` is allocatable.
+! 	This subroutine internally calls [[intrinsicCOPYintrinsic]]. Also `Val`
+! is allocatable.
 !@endnote
 !
 !
 !### Usage
 !
 !```fortran
-  ! integer( i4b ), parameter :: n = 10000
-  ! type( RealVector_ ) :: x, y
-  ! real( dfp ), allocatable :: z( : )
-  ! call random_number( x, n )
-  ! call copy( x = x, y = y )
-  ! call display( asum( x%val - y%val ), "test6: 0 if correct : " )
-  ! call copy( y=z, x=x )
-  ! call display( asum( z - x%val ), "test6: 0 if correct : " )
-  ! call copy( y=x, x=z )
-  ! call display( asum( z - x%val ), "test6: 0 if correct : " )
+! integer( i4b ), parameter :: n = 10000
+! type( RealVector_ ) :: x, y
+! real( dfp ), allocatable :: z( : )
+! call random_number( x, n )
+! call copy( x = x, y = y )
+! call display( asum( x%val - y%val ), "test6: 0 if correct : " )
+! call copy( y=z, x=x )
+! call display( asum( z - x%val ), "test6: 0 if correct : " )
+! call copy( y=x, x=z )
+! call display( asum( z - x%val ), "test6: 0 if correct : " )
 !```
 
 INTERFACE
-MODULE SUBROUTINE intrinsicCOPYscalar( Y, X )
-  REAL( DFP ), ALLOCATABLE, INTENT( INOUT ) :: Y( : )
+MODULE SUBROUTINE intrinsicCOPYscalar_1a( Y, X )
+  REAL( Real32 ), ALLOCATABLE, INTENT( INOUT ) :: Y( : )
   CLASS( RealVector_ ), INTENT( IN ) :: X
-END SUBROUTINE intrinsicCOPYscalar
+END SUBROUTINE intrinsicCOPYscalar_1a
+MODULE SUBROUTINE intrinsicCOPYscalar_1b( Y, X )
+  REAL( Real64 ), ALLOCATABLE, INTENT( INOUT ) :: Y( : )
+  CLASS( RealVector_ ), INTENT( IN ) :: X
+END SUBROUTINE intrinsicCOPYscalar_1b
 END INTERFACE
+
+INTERFACE COPY
+  MODULE PROCEDURE intrinsicCOPYscalar_1a, intrinsicCOPYscalar_1b
+END INTERFACE COPY
 
 !----------------------------------------------------------------------------
 !                                                                 COPY@BLAS1
@@ -388,31 +418,34 @@ END INTERFACE
 ! summary: 	This routine copies one vector into another
 !
 !# Introduction
-! This subroutine copy a vector of [[RealVector_]] into another vector, i.e. `obj1=obj2` see the figure below:
+! This subroutine copy a vector of [[RealVector_]] into another vector, i.e.
+! `obj1=obj2` see the figure below:
 !
-! <img src=|media|/vector_copy_vector.jpg alt="drawing" style="max-width:500px;"/>
+! <img src=|media|/vector_copy_vector.jpg alt="drawing"
+! style="max-width:500px;"/>
 !
 !@note
-! 	This subroutine internally uses [[intrinsicCOPYintrinsic]] routine. Also note that `obj1` and `obj2` are vectors of [[RealVector_]] data type.
+! 	This subroutine internally uses [[intrinsicCOPYintrinsic]] routine. Also
+! note that `obj1` and `obj2` are vectors of [[RealVector_]] data type.
 !@endnote
 !
 !
 !### Usage
 !
 !```fortran
-  ! integer( i4b ), parameter :: n = 10000, m = 5
-  ! type( RealVector_ ), allocatable :: x( : ), y( : )
-  ! integer( i4b ) :: tsize( m ), i
-  ! real( dfp ), allocatable :: z( : )
-  ! real( dfp ) :: ans
-  ! tsize = n
-  ! call random_number(x, tsize)
-  ! call copy( x = x, y = y )
-  ! ans = 0.0
-  ! do i = 1, size( x )
-  !   ans = ans + asum( x(i)%val - y(i)%val )
-  ! end do
-  ! call display( ans, "test7: 0 if correct : " )
+! integer( i4b ), parameter :: n = 10000, m = 5
+! type( RealVector_ ), allocatable :: x( : ), y( : )
+! integer( i4b ) :: tsize( m ), i
+! real( dfp ), allocatable :: z( : )
+! real( dfp ) :: ans
+! tsize = n
+! call random_number(x, tsize)
+! call copy( x = x, y = y )
+! ans = 0.0
+! do i = 1, size( x )
+!   ans = ans + asum( x(i)%val - y(i)%val )
+! end do
+! call display( ans, "test7: 0 if correct : " )
 !```
 
 INTERFACE
@@ -422,6 +455,10 @@ MODULE SUBROUTINE vectorCOPYvector( Y, X )
 END SUBROUTINE vectorCOPYvector
 END INTERFACE
 
+INTERFACE COPY
+  MODULE PROCEDURE vectorCOPYvector
+END INTERFACE COPY
+
 !----------------------------------------------------------------------------
 !                                                                 COPY@BLAS1
 !----------------------------------------------------------------------------
@@ -431,12 +468,15 @@ END INTERFACE
 ! summary: 	This routine copies one vector into another
 !
 !# Introduction
-! This subroutine copies a vector of [[RealVector_]] into a scalar instance of [[RealVector_]]. See Figure below:
+! This subroutine copies a vector of [[RealVector_]] into a scalar instance
+! of [[RealVector_]]. See Figure below:
 !
-! <img src=|media|/scalar_copy_vector.jpg alt="drawing" style="max-width:500px;"/>
+! <img src=|media|/scalar_copy_vector.jpg alt="drawing"
+! style="max-width:500px;"/>
 !
 !@note
-! 	This subroutine internally uses [[intrinsicCOPYintrinsic]] routine.
+! This subroutine internally uses [[intrinsicCOPYintrinsic]] routine.
+!@endnote
 !
 !@todo
 ! need parallel
@@ -449,29 +489,9 @@ MODULE SUBROUTINE scalarCOPYvector( Y, X )
 END SUBROUTINE scalarCOPYvector
 END INTERFACE
 
-!----------------------------------------------------------------------------
-!                                                                 COPY@BLAS1
-!----------------------------------------------------------------------------
-
-!> authors: Vikas Sharma, Ph. D.
-! date: 	26 Feb 2021
-! summary: 	This is generic subroutine for copying.
-!
-!# Introduction
-! See
-! * [[intrinsicCOPYintrinsic]]
-! * [[scalarCOPYscalar]]
-! * [[vectorCOPYvector]]
-! * [[scalarCOPYvector]]
-! * [[scalarCOPYintrinsic]]
-
 INTERFACE COPY
-  MODULE PROCEDURE scalarCOPYscalar, &
-    & vectorCOPYvector, scalarCOPYvector, scalarCOPYintrinsic, &
-    & intrinsicCOPYscalar
+  MODULE PROCEDURE scalarCOPYvector
 END INTERFACE COPY
-
-PUBLIC :: COPY
 
 !----------------------------------------------------------------------------
 !                                                            Compact@BLAS1V
@@ -530,7 +550,8 @@ END INTERFACE
 
 !> authors: Vikas Sharma, Ph. D.
 ! date: 	25 Feb 2021
-! summary: 	This routine computes dot product of a fortran array and scalar instance of [[RealVector_]]
+! summary: 	This routine computes dot product of a fortran array and scalar
+! instance of [[RealVector_]]
 !
 !@todo
 ! type(_obj_) :: obj1
@@ -554,7 +575,8 @@ END INTERFACE
 
 !> authors: Vikas Sharma, Ph. D.
 ! date: 	25 Feb 2021
-! summary: 	This routines returns the dot product of vector of [[RealVector_]] data type.
+! summary: 	This routines returns the dot product of vector of
+! [[RealVector_]] data type.
 !
 !@todo
 ! type(_obj_) :: obj1(2), obj2(2)
@@ -577,7 +599,8 @@ END INTERFACE
 
 !> authors: Vikas Sharma, Ph. D.
 ! date: 	25 Feb 2021
-! summary: 	This routine computes dot product of a vector of [[RealVector_]] and scalar of [[RealVector_]]
+! summary: 	This routine computes dot product of a vector of [[RealVector_]]
+! and scalar of [[RealVector_]]
 !
 !@todo
 ! type(_obj_) :: obj1(2), obj2(2)
@@ -600,7 +623,8 @@ END INTERFACE
 
 !> authors: Vikas Sharma, Ph. D.
 ! date: 	25 Feb 2021
-! summary: 	This routine computes dot product of a scalar of [[RealVector_]] and vector of [[RealVector_]]
+! summary: 	This routine computes dot product of a scalar of [[RealVector_]]
+! and vector of [[RealVector_]]
 !
 !### Usage
 !
