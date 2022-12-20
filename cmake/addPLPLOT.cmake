ecae805199f0320121c1c2e464f877acb41bc564
Vikas Sharma <Vickysharma0812@gmail.com>
2022-12-20 21:36:40 +0900

    Enhancements in the CMake Changes to be committed: 	new file:   cmake/addARPACK.cmake 	new file:   cmake/addGTKFortran.cmake 	modified:   cmake/addPLPLOT.cmake 	modified:   cmake/print_target_properties.cmake

--- cmake/addPLPLOT.cmake
+++ cmake/addPLPLOT.cmake
@@ -15,39 +15,84 @@
 # along with this program.  If not, see <https: //www.gnu.org/licenses/>
 #
 #PLPLOT
+# IF( ${PROJECT_NAME} MATCHES "easifemBase" )
+#   OPTION( USE_PLPLOT OFF )
+#   IF( USE_PLPLOT )
+#     LIST( APPEND TARGET_COMPILE_DEF "-DUSE_PLPLOT" )
+#     IF( UNIX )
+#       IF(APPLE)
+#         SET(PLPLOT_INCLUDE_DIR
+#           "/usr/local/lib/fortran/modules/plplot" )
+#         SET(PLPLOT_LIBRARY
+#           "/usr/local/lib/libplplot.dylib" )
+#         SET(PLPLOT_FORTRAN_LIBRARY
+#           "/usr/local/lib/libplplotfortran.dylib" )
+#       ELSE()
+#         SET(PLPLOT_INCLUDE_DIR
+#           "/usr/lib/x86_64-linux-gnu/fortran/modules/plplot" )
+#         SET(PLPLOT_LIBRARY
+#           "/usr/lib/x86_64-linux-gnu/libplplot.so" )
+#         SET(PLPLOT_FORTRAN_LIBRARY
+#           "/usr/lib/x86_64-linux-gnu/libplplotfortran.so" )
+#       ENDIF()
+#     ENDIF()
+#     TARGET_LINK_LIBRARIES(
+#       ${PROJECT_NAME}
+#       PUBLIC
+#       ${PLPLOT_LIBRARY}
+#       ${PLPLOT_FORTRAN_LIBRARY}
+#       )
+#     TARGET_INCLUDE_DIRECTORIES(
+#       ${PROJECT_NAME}
+#       PUBLIC
+#       ${PLPLOT_INCLUDE_DIR}
+#       )
+#     MESSAGE( STATUS "PLPLOT_LIBRARY : ${PLPLOT_LIBRARY}" )
+#     MESSAGE( STATUS "PLPLOT_FORTRAN_LIBRARY : ${PLPLOT_FORTRAN_LIBRARY}" )
+#     MESSAGE( STATUS "PLPLOT_INCLUDE_DIR : ${PLPLOT_INCLUDE_DIR}" )
+#   ELSE()
+#     MESSAGE( STATUS "NOT USING PLPLOT LIBRARIES" )
+#   ENDIF()
+# ENDIF()
+
+#PLPLOT
 IF( ${PROJECT_NAME} MATCHES "easifemBase" )
   OPTION( USE_PLPLOT OFF )
   IF( USE_PLPLOT )
     LIST( APPEND TARGET_COMPILE_DEF "-DUSE_PLPLOT" )
-    IF( UNIX )
-      IF(APPLE)
-        SET(PLPLOT_INCLUDE_DIR
-          "/usr/local/lib/fortran/modules/plplot" )
-        SET(PLPLOT_LIBRARY
-          "/usr/local/lib/libplplot.dylib" )
-        SET(PLPLOT_FORTRAN_LIBRARY
-          "/usr/local/lib/libplplotfortran.dylib" )
-      ELSE()
-        SET(PLPLOT_INCLUDE_DIR
-          "/usr/lib/x86_64-linux-gnu/fortran/modules/plplot" )
-        SET(PLPLOT_LIBRARY
-          "/usr/lib/x86_64-linux-gnu/libplplot.so" )
-        SET(PLPLOT_FORTRAN_LIBRARY
-          "/usr/lib/x86_64-linux-gnu/libplplotfortran.so" )
-      ENDIF()
-    ENDIF()
+
+    FIND_PACKAGE(PkgConfig REQUIRED)
+    pkg_check_modules(PLPLOT-FORTRAN REQUIRED plplot-fortran)
+    pkg_check_modules(PLPLOT REQUIRED plplot)
+    FIND_LIBRARY(PLPLOT_LIBRARY NAMES plplot)
+    FIND_LIBRARY(PLPLOT-FORTRAN_LIBRARY NAMES plplotfortran)
+    SET(PLPLOT_LIBRARIES ${PLPLOT_LIBRARY} ${PLPLOT-FORTRAN_LIBRARY})
+    FIND_PATH(PLPLOT_INCLUDE_DIR NAMES plplot/plplot.h)
+    FIND_PATH(
+      PLPLOT_MODULE_DIR
+      NAMES plplot.mod
+      PATHS ${PLPLOT-FORTRAN_INCLUDE_DIRS})
+    INCLUDE(FindPackageHandleStandardArgs)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(
+      PLPLOT DEFAULT_MSG
+      PLPLOT_LIBRARIES
+      PLPLOT_INCLUDE_DIR
+      )
+
+    SET(PLPLOT_FORTRAN_INCLUDE_DIR "${PLPLOT_MODULE_DIR}")
+
     TARGET_LINK_LIBRARIES(
       ${PROJECT_NAME}
       PUBLIC
-      ${PLPLOT_LIBRARY}
-      ${PLPLOT_FORTRAN_LIBRARY}
+      ${PLPLOT_LIBRARIES}
       )
     TARGET_INCLUDE_DIRECTORIES(
       ${PROJECT_NAME}
       PUBLIC
       ${PLPLOT_INCLUDE_DIR}
+      ${PLPLOT_FORTRAN_INCLUDE_DIR}
       )
-    MESSAGE( STATUS "PLPLOT_LIBRARY : ${PLPLOT_LIBRARY}" )
+    MESSAGE( STATUS "PLPLOT_LIBRARIES : ${PLPLOT_LIBRARIES}" )
     MESSAGE( STATUS "PLPLOT_FORTRAN_LIBRARY : ${PLPLOT_FORTRAN_LIBRARY}" )
     MESSAGE( STATUS "PLPLOT_INCLUDE_DIR : ${PLPLOT_INCLUDE_DIR}" )
   ELSE()
