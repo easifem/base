644d7acfe658f8ae0d1908d3c1b40ca36b595d52
Vikas Sharma <Vickysharma0812@gmail.com>
2022-12-20 21:37:17 +0900

    Adding ARPACK module. Changes to be committed: 	new file:   src/modules/ARPACK/CMakeLists.txt 	new file:   src/modules/ARPACK/src/ARPACK_SAUPD.F90 	new file:   src/modules/ARPACK/src/EASIFEM_ARPACK.F90 	new file:   src/modules/ARPACK/src/EASIFEM_F77_ARPACK.F90

---
+++ src/modules/ARPACK/src/EASIFEM_ARPACK.F90
@@ -0,0 +1,25 @@
+! This program is a part of EASIFEM library
+! Copyright (C) 2020-2021  Vikas Sharma, Ph.D
+!
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <https: //www.gnu.org/licenses/>
+!
+
+!> author: Vikas Sharma, Ph. D.
+! date: 2022-12-09
+! summary: This module contains interface to ARPACK lib.
+
+MODULE EASIFEM_ARPACK
+USE EASIFEM_F77_ARPACK
+USE ARPACK_SAUPD
+END MODULE EASIFEM_ARPACK
