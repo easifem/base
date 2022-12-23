ec8ee7b0f076c314c2970c4a242d0c86b9a1f66e
Vikas Sharma <Vickysharma0812@gmail.com>
2022-12-23 14:38:24 +0900

    LDL fixed in Sym_Lapack_Method Changes to be committed: 	modified:   src/submodules/Lapack/src/Sym_Lapack_Method@LUMethods.F90

--- src/submodules/Lapack/src/Sym_Lapack_Method@LUMethods.F90
+++ src/submodules/Lapack/src/Sym_Lapack_Method@LUMethods.F90
@@ -16,7 +16,8 @@
 !
 
 SUBMODULE(Sym_Lapack_Method) LUMethods
-USE BaseMethod, ONLY: Display, Input, Arange, Zeros, GetTril, GetTriu, ArgSort
+USE BaseMethod, ONLY: Display, Input, Arange, Zeros, GetTril, &
+& GetTriu, ArgSort, tostring
 USE F95_LAPACK, ONLY: SYTRF, LACPY, LAPMR, POTRF, SYTRS, SYTRI
 USE F77_LAPACK, ONLY: SYCONV => LA_SYCONV
 IMPLICIT NONE
@@ -45,28 +46,41 @@ END PROCEDURE SymGetLU_2
 
 MODULE PROCEDURE SymGetLDL_1
 CHARACTER(LEN=1) :: luplo
-INTEGER(I4B) :: linfo, n, ii
-INTEGER(I4B), ALLOCATABLE :: ipiv0(:), swap_(:), pivots(:)
-REAL(DFP), ALLOCATABLE :: dlu(:, :)
-
+INTEGER(I4B) :: linfo, n
+INTEGER(I4B), ALLOCATABLE :: work(:, :)
+!
+! work(1:n, 1) = ipiv(:) from sytrf
+! work(1:n, 2) = swap_(:) block diagonal information
+! work(1:n, 3) = pivots(:) cleaned version of ipiv
+! work(1:n, 4) = perm cleaned version of ipiv
+!
 n = SIZE(A, 1)
-ALLOCATE (ipiv0(n), swap_(n), pivots(n))
-
+ALLOCATE (work(n, 4))
 luplo = INPUT(option=UPLO, default="U")
-
 LU = 0.0_DFP
+!
+! Copy data in LU from  A
+!
 CALL LACPY(A=A, B=LU, UPLO=luplo)
-CALL SYTRF(A=LU, UPLO=luplo, IPIV=ipiv0, INFO=linfo)
-CALL LDL_SENITIZE_IPIV(ipiv0, luplo, swap_, pivots)
-dlu = lu
-CALL LDL_GET_D_and_L(D, E, lu, dlu, pivots, luplo)
-CALL LDL_CONSTRUCT_TRI_FACTOR(lu, swap_, pivots, ipiv0, luplo)
-STOP
-
-CALL LAPMR(X=LU, K=ipiv0, FORWRD=.TRUE.)
+!
+! Call SYTRF
+!
+CALL SYTRF(A=LU, UPLO=luplo, IPIV=work(:, 1), INFO=linfo)
+!
+! Clean the ipiv0 returned by SYTRF
+!
+CALL LDL_SENITIZE_IPIV(ipiv0=work(:, 1), uplo=luplo, &
+  & swap_=work(:, 2), pivots=work(:, 3), info=linfo)
+!
+CALL LDL_GET_D_and_L(D=D, E=E, ldu=lu, &
+  & pivs=work(:, 3), uplo=luplo)
+!
+CALL LDL_CONSTRUCT_TRI_FACTOR(lu=lu, swap_=work(:, 2), &
+  & pivots=work(:, 3), perm=work(:, 4), permTemp=work(:, 1), uplo=luplo)
+!
 IF (PRESENT(info)) info = linfo
-IF (PRESENT(IPIV)) IPIV = ipiv0
-DEALLOCATE (ipiv0)
+IF (PRESENT(IPIV)) IPIV = work(:, 4)
+DEALLOCATE (work)
 END PROCEDURE SymGetLDL_1
 
 !----------------------------------------------------------------------------
@@ -116,17 +130,19 @@ END PROCEDURE SymGetLDL_1
 !     pivots : ndarray
 !         The array that defines the block diagonal structure as given above.
 
-SUBROUTINE LDL_SENITIZE_IPIV(ipiv0, uplo, swap_, pivots)
+SUBROUTINE LDL_SENITIZE_IPIV(ipiv0, uplo, swap_, pivots, info)
   INTEGER(I4B), INTENT(IN) :: ipiv0(:)
   CHARACTER(LEN=1), INTENT(IN) :: uplo
-  INTEGER(I4B), INTENT(OUT) :: swap_(:)
-  INTEGER(I4B), INTENT(OUT) :: pivots(:)
+  INTEGER(I4B), INTENT(INOUT) :: swap_(:)
+  INTEGER(I4B), INTENT(INOUT) :: pivots(:)
+  INTEGER(I4B), INTENT(OUT) :: info
   !
   ! internal variables
   !
   INTEGER(I4B) :: n, ind, x, y, rs, re, ri, cur_val
   LOGICAL(LGT) :: skip2x2
   !
+  info = 0
   n = SIZE(ipiv0)
   !
   IF (uplo .EQ. "L") THEN
@@ -142,33 +158,33 @@ SUBROUTINE LDL_SENITIZE_IPIV(ipiv0, uplo, swap_, pivots)
     re = 1
     ri = -1
   END IF
+  !
   skip2x2 = .FALSE.
   swap_ = arange(1_I4B, n)
   pivots = zeros(n, 1_I4B)
-
+  !
   DO ind = rs, re, ri
     IF (skip2x2) skip2x2 = .FALSE.
     cur_val = ipiv0(ind)
-
-    if (cur_val .GT. 0) then
-      if (cur_val .NE. ind) then
+    !
+    IF (cur_val .GT. 0) THEN
+      IF (cur_val .NE. ind) THEN
         swap_(ind) = swap_(cur_val)
-      end if
+      END IF
       pivots(ind) = 1
-    elseif (cur_val < 0 .AND. cur_val == ipiv0(ind + x)) then
-      if (-cur_val .NE. ind + 1) then
+      !
+    ELSEIF (cur_val < 0 .AND. cur_val == ipiv0(ind + x)) THEN
+      !
+      IF (-cur_val .NE. ind + 1) THEN
         swap_(ind + x) = swap_(-cur_val)
-      end if
+      END IF
+      !
       pivots(ind + y) = 2
       skip2x2 = .TRUE.
-    else
-    !! report error
-    end if
+      !
+    END IF
   END DO
 
-  CALL Display(ipiv0, "ipiv0 = ")
-  CALL Display(swap_, "swap_ = ")
-  CALL Display(pivots, "pivots = ")
 END SUBROUTINE LDL_SENITIZE_IPIV
 
 !----------------------------------------------------------------------------
@@ -183,28 +199,33 @@ END SUBROUTINE LDL_SENITIZE_IPIV
 !
 ! Helper function to extract the diagonal and triangular matrices for
 ! LDL.T factorization.
-! Parameters
-! ----------
+!
+!## Parameters
+!
 ! ldu : ndarray
 !     The compact output returned by the LAPACK routing
+!
 ! pivs : ndarray
 !     The sanitized array of {0, 1, 2} denoting the sizes of the pivots. For
 !     every 2 there is a succeeding 0.
+!
 ! lower : bool, optional
 !     If set to False, upper triangular part is considered.
+!
 ! hermitian : bool, optional
 !     If set to False a symmetric complex array is assumed.
-! Returns
-! -------
+!
+!## Returns
+!
 ! d : ndarray
 !     The block diagonal matrix.
+!
 ! lu : ndarray
 !     The upper/lower triangular matrix
 
-SUBROUTINE LDL_GET_D_and_L(D, E, lu, ldu, pivs, uplo)
-  REAL(DFP), INTENT(OUT) :: D(:)
-  REAL(DFP), INTENT(OUT) :: E(:)
-  REAL(DFP), INTENT(INOUT) :: lu(:, :)
+SUBROUTINE LDL_GET_D_and_L(D, E, ldu, pivs, uplo)
+  REAL(DFP), INTENT(INOUT) :: D(:)
+  REAL(DFP), INTENT(INOUT) :: E(:)
   REAL(DFP), INTENT(INOUT) :: ldu(:, :)
   INTEGER(I4B), INTENT(IN) :: pivs(:)
   CHARACTER(LEN=1), INTENT(IN) :: uplo
@@ -215,10 +236,12 @@ SUBROUTINE LDL_GET_D_and_L(D, E, lu, ldu, pivs, uplo)
   !
   ! extract D from LDU
   !
-  n = SIZE(pivs)
+  n = SIZE(ldu, 1)
   !
   DO CONCURRENT(ii=1:n)
     D(ii) = ldu(ii, ii)
+    E(ii) = 0.0_DFP
+    ldu(ii, ii) = 1.0_DFP
   END DO
   !
   blk_i = 1
@@ -226,21 +249,16 @@ SUBROUTINE LDL_GET_D_and_L(D, E, lu, ldu, pivs, uplo)
   IF (uplo .EQ. "L") THEN
     x = 1
     y = 0
-    CALL GetTril(A=ldu, diagNo=-1, lu=lu)
-    !!
-    DO CONCURRENT(ii=1:n)
-      lu(ii, ii) = 1.0_DFP
-    END DO
     !!
     DO ii = 1, n
       IF (pivs(ii) .EQ. 0) CYCLE
       ! increment the block index and check for 2s
       ! if 2 then copy the off diagonals depending on uplo
-      inc = blk_i + ii
+      inc = blk_i + pivs(ii)
       !
       IF (pivs(ii) .EQ. 2) THEN
         E(blk_i) = ldu(blk_i + x, blk_i + y)
-        lu(blk_i + x, blk_i + y) = 0.0
+        ldu(blk_i + x, blk_i + y) = 0.0
       END IF
       !
       blk_i = inc
@@ -250,31 +268,22 @@ SUBROUTINE LDL_GET_D_and_L(D, E, lu, ldu, pivs, uplo)
   ELSE
     y = 1
     x = 0
-    CALL GetTriu(A=ldu, diagNo=1, lu=lu)
-    !!
-    DO CONCURRENT(ii=1:n)
-      lu(ii, ii) = 1.0_DFP
-    END DO
     !!
     DO ii = 1, n
       IF (pivs(ii) .EQ. 0) CYCLE
       ! increment the block index and check for 2s
       ! if 2 then copy the off diagonals depending on uplo
-      inc = blk_i + ii
+      inc = blk_i + pivs(ii)
       !
       IF (pivs(ii) .EQ. 2) THEN
         E(blk_i) = ldu(blk_i + x, blk_i + y)
-        lu(blk_i + x, blk_i + y) = 0.0
+        ldu(blk_i + x, blk_i + y) = 0.0
       END IF
       !
       blk_i = inc
       !
     END DO
   END IF
-  !
-  CALL Display(D, "D = ")
-  CALL Display(E, "E = ")
-  CALL Display(LU, "LU = ")
   !
 END SUBROUTINE LDL_GET_D_and_L
 
@@ -297,11 +306,11 @@ END SUBROUTINE LDL_GET_D_and_L
 ! lu : ndarray
 ! The triangular array that is extracted from LAPACK routine call with
 ! ones on the diagonals.
-! swap_vec : ndarray
+! swap_ : ndarray
 ! The array that defines the row swapping indices. If the kth entry is m
 ! then rows k,m are swapped. Notice that the mth entry is not necessarily
 ! k to avoid undoing the swapping.
-! pivs : ndarray
+! pivots : ndarray
 ! The array that defines the block diagonal structure returned by
 ! _ldl_sanitize_ipiv().
 ! lower : bool, optional
@@ -317,11 +326,12 @@ END SUBROUTINE LDL_GET_D_and_L
 !Note that the original argument "lu" is overwritten.
 !@endnote
 
-SUBROUTINE LDL_CONSTRUCT_TRI_FACTOR(lu, swap_vec, pivs, perm, uplo)
+SUBROUTINE LDL_CONSTRUCT_TRI_FACTOR(lu, swap_, pivots, perm, permTemp, uplo)
   REAL(DFP), INTENT(INOUT) :: lu(:, :)
-  INTEGER(I4B), INTENT(IN) :: swap_vec(:)
-  INTEGER(I4B), INTENT(IN) :: pivs(:)
+  INTEGER(I4B), INTENT(IN) :: swap_(:)
+  INTEGER(I4B), INTENT(IN) :: pivots(:)
   INTEGER(I4B), INTENT(INOUT) :: perm(:)
+  INTEGER(I4B), INTENT(INOUT) :: permTemp(:)
   CHARACTER(LEN=1), INTENT(IN) :: uplo
   !
   ! internal variables
@@ -333,7 +343,7 @@ SUBROUTINE LDL_CONSTRUCT_TRI_FACTOR(lu, swap_vec, pivs, perm, uplo)
   ! main program
   !
   n = SIZE(lu, 1)
-  perm = arange(1_I4B, n, 1_I4B)
+  permTemp = arange(1_I4B, n, 1_I4B)
   !
   IF (uplo .EQ. "L") THEN
     rs = n
@@ -354,32 +364,32 @@ SUBROUTINE LDL_CONSTRUCT_TRI_FACTOR(lu, swap_vec, pivs, perm, uplo)
   END IF
   !
   DO ind = rs, re, ri
-    s_ind = swap_vec(ind)
+    s_ind = swap_(ind)
+    !
     IF (s_ind .NE. ind) THEN
       ! Column start and end positions
       IF (islower) THEN
-        col_s = ind + 1
+        col_s = ind
         col_e = n
       ELSE
         col_s = 1
-        col_e = ind + 1
+        col_e = ind
       END IF
       !
-      ! If we stumble upon a 2x2 block include both cols in the perm.
+      ! If we stumble upon a 2x2 block include both cols in the permTemp.
       !
-      IF (pivs(ind) .EQ. zero_or_two) THEN
+      IF (pivots(ind) .EQ. zero_or_two) THEN
         col_s = col_s + incr_col_s
         col_e = col_e + incr_col_e
       END IF
       !
       lu([s_ind, ind], col_s:col_e) = lu([ind, s_ind], col_s:col_e)
-      perm([s_ind, ind]) = perm([ind, s_ind])
+      permTemp([s_ind, ind]) = permTemp([ind, s_ind])
       !
     END IF
   END DO
   !!
-  CALL Display(lu(ArgSort(perm), :), " lu = ")
-  CALL Display(ArgSort(perm), " perm = ")
+  perm = ArgSort(permTemp)
   !!
 END SUBROUTINE LDL_CONSTRUCT_TRI_FACTOR
 
@@ -388,46 +398,37 @@ END SUBROUTINE LDL_CONSTRUCT_TRI_FACTOR
 !----------------------------------------------------------------------------
 
 MODULE PROCEDURE SymGetLDL_2
-INTEGER(I4B) :: n, ii
-INTEGER(I4B), ALLOCATABLE :: ipiv0(:)
 CHARACTER(LEN=1) :: luplo
-
-luplo = INPUT(default="U", option=UPLO)
-
+INTEGER(I4B) :: linfo, n
+INTEGER(I4B), ALLOCATABLE :: work(:, :)
+!
+! work(1:n, 1) = ipiv(:) from sytrf
+! work(1:n, 2) = swap_(:) block diagonal information
+! work(1:n, 3) = pivots(:) cleaned version of ipiv
+! work(1:n, 4) = perm cleaned version of ipiv
+!
 n = SIZE(A, 1)
-ALLOCATE (ipiv0(n))
-
-! make some entries of A to zero
-
-IF (luplo .EQ. "U") THEN
-  DO CONCURRENT(ii=1:n - 1)
-    A(ii + 1:, ii) = 0.0_DFP
-  END DO
-ELSE
-  DO CONCURRENT(ii=2:n)
-    A(:ii - 1, ii) = 0.0_DFP
-  END DO
-END IF
-
-CALL SYTRF(A=A, UPLO=uplo, IPIV=ipiv0, INFO=info)
-CALL SYCONV(UPLO=uplo, WAY="C", N=n, A=A, LDA=n, &
-  & IPIV=ipiv0, E=E, INFO=info)
-
-DO CONCURRENT(ii=1:n)
-  D(ii) = A(ii, ii)
-  A(ii, ii) = 1.0_DFP
-END DO
-
-DO ii = 1, n
-  IF (ipiv0(ii) .GT. 0 .AND. ipiv0(ii) .NE. ii) THEN
-    ipiv0(ipiv0(ii)) = ii
-  END IF
-END DO
-
-CALL LAPMR(X=A, K=ipiv0, FORWRD=.TRUE.)
-
-IF (PRESENT(IPIV)) IPIV = ipiv0
-DEALLOCATE (ipiv0)
+ALLOCATE (work(n, 4))
+luplo = INPUT(option=UPLO, default="U")
+!
+! Call SYTRF
+!
+CALL SYTRF(A=A, UPLO=luplo, IPIV=work(:, 1), INFO=info)
+!
+! Clean the ipiv0 returned by SYTRF
+!
+CALL LDL_SENITIZE_IPIV(ipiv0=work(:, 1), uplo=luplo, &
+  & swap_=work(:, 2), pivots=work(:, 3), info=linfo)
+!
+CALL LDL_GET_D_and_L(D=D, E=E, ldu=A, &
+  & pivs=work(:, 3), uplo=luplo)
+!
+CALL LDL_CONSTRUCT_TRI_FACTOR(lu=A, swap_=work(:, 2), &
+  & pivots=work(:, 3), perm=work(:, 4), permTemp=work(:, 1), uplo=luplo)
+!
+IF (PRESENT(info)) info = linfo
+IF (PRESENT(IPIV)) IPIV = work(:, 4)
+DEALLOCATE (work)
 END PROCEDURE SymGetLDL_2
 
 !----------------------------------------------------------------------------
