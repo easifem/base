ea3c40b8756fd36113bbddb1326b446f97668037
Vikas Sharma <vickysharma0812@gmail.com>
2023-08-15 21:47:52 +0900

    Update: MdEncode_Method
    Adding: New methods to MdEncode_Method

--- src/submodules/MdEncode/src/MdEncode_Method@Methods.F90
+++ src/submodules/MdEncode/src/MdEncode_Method@Methods.F90
@@ -165,4 +165,280 @@ DO jj = 1, SIZE(val, 4)
 END DO
 END PROCEDURE MdEncode_5
 
+!----------------------------------------------------------------------------
+!                                                               Mdencode
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE MdEncode_6
+INTEGER(I4B) :: nc, nr, n, ii
+nc = SIZE(ch)
+nr = SIZE(rh)
+n = SIZE(val)
+
+SELECT CASE (nc)
+CASE (1)
+
+  IF (nr .EQ. n) THEN
+
+    ans = ivert//avert//ch(1)%chars()//evert//abr
+    ans = ans// &
+        & ivert//adash//avert//adash//evert//abr
+
+    DO ii = 1, n
+      ans = ans// &
+          & ivert//rh(ii)%chars()//avert//mdencode(val(ii))//evert//abr
+    END DO
+    ans = ans//abr
+
+  ELSE
+
+    IF (ch(1)%LEN_TRIM() .EQ. 0_I4B) THEN
+
+      ans = ivert//avert
+
+      DO ii = 1, n
+        ans = ans//ablank//evert
+      END DO
+      ans = ans//abr
+
+      ans = ans//ivert//adash//evert
+      DO ii = 1, n
+        ans = ans//adash//evert
+      END DO
+      ans = ans//abr
+
+      ans = ans//ivert//rh(1)%chars()//evert
+
+      DO ii = 1, n
+        ans = ans//mdencode(val(ii))//evert
+      END DO
+      ans = ans//abr
+    ELSE
+
+      ans = ivert//ch(1)%chars()//evert//abr
+      ans = ans// &
+          & ivert//adash//evert//abr
+
+      DO ii = 1, n
+        ans = ans// &
+            & ivert//mdencode(val(ii))//evert//abr
+      END DO
+      ans = ans//abr
+    END IF
+
+  END IF
+
+CASE default
+
+  IF (nc .EQ. n) THEN
+
+    ans = ivert//avert
+
+    DO ii = 1, n
+      ans = ans//ch(ii)%chars()//evert
+    END DO
+    ans = ans//abr
+
+    ans = ans//ivert//adash//evert
+    DO ii = 1, n
+      ans = ans//adash//evert
+    END DO
+    ans = ans//abr
+
+    ans = ans//ivert//rh(1)%chars()//evert
+
+    DO ii = 1, n
+      ans = ans//mdencode(val(ii))//evert
+    END DO
+    ans = ans//abr
+
+  ELSE
+
+    ans = ivert//avert
+
+    DO ii = 1, n
+      ans = ans//ablank//evert
+    END DO
+    ans = ans//abr
+
+    ans = ans//ivert//adash//evert
+    DO ii = 1, n
+      ans = ans//adash//evert
+    END DO
+    ans = ans//abr
+
+    ans = ans//ivert//rh(1)%chars()//evert
+
+    DO ii = 1, n
+      ans = ans//mdencode(val(ii))//evert
+    END DO
+    ans = ans//abr
+
+  END IF
+
+END SELECT
+
+END PROCEDURE MdEncode_6
+
+!----------------------------------------------------------------------------
+!                                                           MdEncode
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE MdEncode_7
+INTEGER(I4B) :: nc, nr, n, ii, m, jj
+LOGICAL(LGT) :: norow, nocol
+nc = SIZE(ch)
+nr = SIZE(rh)
+m = SIZE(val, 1)
+n = SIZE(val, 2)
+
+IF (m .EQ. 1) THEN
+  ans = MdEncode(val=val(1, :), rh=rh, ch=ch)
+  RETURN
+END IF
+
+IF (n .EQ. 1) THEN
+  ans = MdEncode(val=val(:, 1), rh=rh, ch=ch)
+  RETURN
+END IF
+
+IF (nc .LT. n) THEN
+  nocol = .TRUE.
+ELSE
+  nocol = .FALSE.
+END IF
+
+IF (nr .LT. m) THEN
+  norow = .TRUE.
+ELSE
+  norow = .FALSE.
+END IF
+
+IF (nocol .AND. norow) THEN
+  ans = MdEncode(val)
+  RETURN
+END IF
+
+IF (norow .AND. (.NOT. nocol)) THEN
+  ! | col-1 | col-2 | col-3 |
+  ! | ---- | ----- | ----- |
+  ! |  1  |   2   |     3 |
+
+  ans = ivert
+
+  DO ii = 1, n
+    ans = ans//ch(ii)%chars()//avert
+  END DO
+  ans = ans//abr
+
+  ans = ans//ivert
+  DO ii = 1, n
+    ans = ans//adash//avert
+  END DO
+  ans = ans//abr
+
+  ans = ans//ivert
+
+  DO ii = 1, m
+    DO jj = 1, n
+      ans = ans//mdencode(val(ii, jj))//avert
+    END DO
+    ans = ans//abr
+  END DO
+  ans = ans//abr
+  RETURN
+END IF
+
+IF (nocol .AND. (.NOT. norow)) THEN
+  ! |       |      |       |       |
+  ! | ----- | ---- | ----- | ----- |
+  ! | row-1 |  1  |   2   |     3  |
+  ! | row-2 |  1  |   2   |     3  |
+  ! | row-3 |  1  |   2   |     3 |
+
+  ans = ivert//avert
+  DO ii = 1, n
+    ans = ans//ablank//avert
+  END DO
+  ans = ans//abr
+
+  ans = ans//ivert//adash//avert
+  DO ii = 1, n
+    ans = ans//adash//avert
+  END DO
+  ans = ans//abr
+
+  DO ii = 1, m
+    ans = ans//ivert//rh(ii)%chars()//avert
+    DO jj = 1, n
+      ans = ans//mdencode(val(ii, jj))//avert
+    END DO
+    ans = ans//abr
+  END DO
+  ans = ans//abr
+  RETURN
+END IF
+
+! |     | col-1 | col-2 | col-3 |
+! | ----- | ---- | ----- | ----- |
+! | row-1 |  1  |   2   |     3  |
+! | row-2 |  1  |   2   |     3  |
+! | row-3 |  1  |   2   |     3 |
+
+ans = ivert//avert
+DO ii = 1, n
+  ans = ans//ch(ii)%chars()//avert
+END DO
+ans = ans//abr
+
+ans = ans//ivert//adash//avert
+DO ii = 1, n
+  ans = ans//adash//avert
+END DO
+ans = ans//abr
+
+DO ii = 1, m
+  ans = ans//ivert//rh(ii)%chars()//avert
+  DO jj = 1, n
+    ans = ans//mdencode(val(ii, jj))//avert
+  END DO
+  ans = ans//abr
+END DO
+ans = ans//abr
+
+END PROCEDURE MdEncode_7
+
+!----------------------------------------------------------------------------
+!                                                           StartTab
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE React_StartTabs
+ans = "<Tabs>"//char_lf
+END PROCEDURE React_StartTabs
+
+!----------------------------------------------------------------------------
+!                                                           EndTabs
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE React_EndTabs
+ans = "</Tabs>"//char_lf
+END PROCEDURE React_EndTabs
+
+!----------------------------------------------------------------------------
+!                                                         StartTabItem
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE React_StartTabItem
+ans = "<TabItem value="//'"'//TRIM(VALUE)//'"'  &
+& //" label="//'"'//TRIM(label)//'" >'//char_lf
+END PROCEDURE React_StartTabItem
+
+!----------------------------------------------------------------------------
+!                                                         StartTabItem
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE React_EndTabItem
+ans = "</TabItem>"//char_lf
+END PROCEDURE React_EndTabItem
+
 END SUBMODULE Methods
