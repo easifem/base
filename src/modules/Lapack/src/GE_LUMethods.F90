9b8c62d68f622a7c9644a8379114cb150d740b51
Vikas Sharma <vickysharma0812@gmail.com>
2023-11-21 00:36:58 +0900

    Update in Lapack methods

--- src/modules/Lapack/src/GE_LUMethods.F90
+++ src/modules/Lapack/src/GE_LUMethods.F90
@@ -20,23 +20,23 @@ USE GlobalData, ONLY: DFP, I4B, LGT
 IMPLICIT NONE
 PRIVATE
 
-PUBLIC :: getLU
+PUBLIC :: GetLU
 PUBLIC :: LUSolve
 PUBLIC :: Inv
 
 !----------------------------------------------------------------------------
-!                                                                   getLU@LU
+!                                                                   GetLU@LU
 !----------------------------------------------------------------------------
 
 !> author: Vikas Sharma, Ph. D.
 ! date: 2022-07-07
-! summary: DGETF2 computes an LU factorization of a general m-by-n matrix A
+! summary: DGetF2 computes an LU factorization of a general m-by-n matrix A
 !
 !# Introduction
 !
 !  Using partial pivoting with row interchanges.
 !
-! DGETF2 computes an LU factorization of a general m-by-n matrix A
+! DGetF2 computes an LU factorization of a general m-by-n matrix A
 !  using partial pivoting with row interchanges.
 !
 !  The factorization has the form
@@ -47,9 +47,9 @@ PUBLIC :: Inv
 !
 !  This is the right-looking Level 2 BLAS version of the algorithm.
 !
-! ## GETRF
+! ## GetRF
 !
-! DGETRF computes an LU factorization of a general M-by-N matrix A
+! DGetRF computes an LU factorization of a general M-by-N matrix A
 !  using partial pivoting with row interchanges
 !
 ! - iterative version of Sivan Toledo's recursive LU algorithm
@@ -62,8 +62,8 @@ PUBLIC :: Inv
 !  triangular (upper trapezoidal if m < n).
 !
 
-INTERFACE
-  MODULE SUBROUTINE getLU_1(A, LU, IPIV, RCOND, NORM, info)
+INTERFACE GetLU
+  MODULE SUBROUTINE GetLU_1(A, LU, IPIV, RCOND, NORM, info)
     REAL(DFP), INTENT(IN) :: A(:, :)
     !! Matrix to be factored
     REAL(DFP), INTENT(OUT) :: LU(:, :)
@@ -79,12 +79,8 @@ INTERFACE
     CHARACTER(1), OPTIONAL, INTENT(IN) :: NORM
     !! NORM "1", "0"
     INTEGER(I4B), OPTIONAL, INTENT(OUT) :: info
-  END SUBROUTINE getLU_1
-END INTERFACE
-
-INTERFACE getLU
-  MODULE PROCEDURE getLU_1
-END INTERFACE getLU
+  END SUBROUTINE GetLU_1
+END INTERFACE GetLU
 
 !----------------------------------------------------------------------------
 !
@@ -96,11 +92,11 @@ END INTERFACE getLU
 !
 !# Introduction
 !
-! This routine is same as `getLU_1` however in this routine LU
+! This routine is same as `GetLU_1` however in this routine LU
 ! factorization is computed in A matrix on return.
 
-INTERFACE
-  MODULE SUBROUTINE getLU_2(A, IPIV, RCOND, NORM, info)
+INTERFACE GetLU
+  MODULE SUBROUTINE GetLU_2(A, IPIV, RCOND, NORM, info)
     REAL(DFP), INTENT(INOUT) :: A(:, :)
     !! Matrix to be factored, on return it contains LU factorization,
     !! the unit diagonal elements of L are not stored.
@@ -115,12 +111,8 @@ INTERFACE
     CHARACTER(1), OPTIONAL, INTENT(IN) :: NORM
     !! "1", "0"
     INTEGER(I4B), OPTIONAL, INTENT(OUT) :: info
-  END SUBROUTINE getLU_2
-END INTERFACE
-
-INTERFACE getLU
-  MODULE PROCEDURE getLU_2
-END INTERFACE getLU
+  END SUBROUTINE GetLU_2
+END INTERFACE GetLU
 
 !----------------------------------------------------------------------------
 !                                                                 LUSolve
@@ -130,25 +122,21 @@ END INTERFACE getLU
 ! date: 28 July 2022
 ! summary: Solve LUx=y
 
-INTERFACE
+INTERFACE LUSolve
   MODULE SUBROUTINE LUSolve_1(A, B, IPIV, isTranspose, info)
     REAL(DFP), INTENT(INOUT) :: A(:, :)
-    !! LU decomposition of matrix A, see getLU
+    !! LU decomposition of matrix A, see GetLU
     REAL(DFP), INTENT(INOUT) :: B(:)
     !! at entry RHS
     !! on return solution will be in B
     INTEGER(I4B), INTENT(IN) :: IPIV(:)
-    !! IPIV returned from getLU
+    !! IPIV returned from GetLU
     LOGICAL(LGT), OPTIONAL, INTENT(IN) :: isTranspose
     !! IF isTranspose true then we solve A^Tx=y
     !! Default is `.false.`
     INTEGER(I4B), OPTIONAL, INTENT(OUT) :: info
     !! info
   END SUBROUTINE LUSolve_1
-END INTERFACE
-
-INTERFACE LUSolve
-  MODULE PROCEDURE LUSolve_1
 END INTERFACE LUSolve
 
 !----------------------------------------------------------------------------
@@ -159,22 +147,18 @@ END INTERFACE LUSolve
 ! date: 28 July 2022
 ! summary: Solve LUx=y
 
-INTERFACE
+INTERFACE LUSolve
   MODULE SUBROUTINE LUSolve_2(A, B, IPIV, isTranspose, info)
     REAL(DFP), INTENT(INOUT) :: A(:, :)
-    !! LU Decomposition of A returned from getLU
+    !! LU Decomposition of A returned from GetLU
     REAL(DFP), INTENT(INOUT) :: B(:, :)
     !! Several rhs, on return solution will be in B
     INTEGER(I4B), INTENT(IN) :: IPIV(:)
-    !! pivoting returned from getLU
+    !! pivoting returned from GetLU
     LOGICAL(LGT), OPTIONAL, INTENT(IN) :: isTranspose
     !! if true we solve A^Tx = y
     INTEGER(I4B), OPTIONAL, INTENT(OUT) :: info
   END SUBROUTINE LUSolve_2
-END INTERFACE
-
-INTERFACE LUSolve
-  MODULE PROCEDURE LUSolve_2
 END INTERFACE LUSolve
 
 !----------------------------------------------------------------------------
@@ -185,25 +169,21 @@ END INTERFACE LUSolve
 ! date: 28 July 2022
 ! summary: Solve LUx=y
 
-INTERFACE
+INTERFACE LUSolve
   MODULE SUBROUTINE LUSolve_3(X, A, B, IPIV, isTranspose, info)
     REAL(DFP), INTENT(OUT) :: X(:)
     !! RHS, on return solution will be in B
     REAL(DFP), INTENT(INOUT) :: A(:, :)
-    !! LU decomposition of matrix A, see getLU
+    !! LU decomposition of matrix A, see GetLU
     REAL(DFP), INTENT(IN) :: B(:)
     !! RHS
     INTEGER(I4B), INTENT(IN) :: IPIV(:)
-    !! IPIV returned from getLU
+    !! IPIV returned from GetLU
     LOGICAL(LGT), OPTIONAL, INTENT(IN) :: isTranspose
     !! if isTranspose true then we solve A^Tx=y
     INTEGER(I4B), OPTIONAL, INTENT(OUT) :: info
     !! info
   END SUBROUTINE LUSolve_3
-END INTERFACE
-
-INTERFACE LUSolve
-  MODULE PROCEDURE LUSolve_3
 END INTERFACE LUSolve
 
 !----------------------------------------------------------------------------
@@ -214,70 +194,58 @@ END INTERFACE LUSolve
 ! date: 28 July 2022
 ! summary: Solve LUx=y
 
-INTERFACE
+INTERFACE LUSolve
   MODULE SUBROUTINE LUSolve_4(X, A, B, IPIV, isTranspose, info)
     REAL(DFP), INTENT(OUT) :: X(:, :)
     !! solution
     REAL(DFP), INTENT(INOUT) :: A(:, :)
-    !! LU Decomposition of A returned from getLU
+    !! LU Decomposition of A returned from GetLU
     REAL(DFP), INTENT(IN) :: B(:, :)
     !! several RHS
     INTEGER(I4B), INTENT(IN) :: IPIV(:)
-    !! pivoting returned from getLU
+    !! pivoting returned from GetLU
     LOGICAL(LGT), OPTIONAL, INTENT(IN) :: isTranspose
     !! if true we solve A^Tx = y
     INTEGER(I4B), OPTIONAL, INTENT(OUT) :: info
   END SUBROUTINE LUSolve_4
-END INTERFACE
-
-INTERFACE LUSolve
-  MODULE PROCEDURE LUSolve_4
 END INTERFACE LUSolve
 
 !----------------------------------------------------------------------------
-!                                                                    getInv
+!                                                                    GetInv
 !----------------------------------------------------------------------------
 
 !> author: Vikas Sharma, Ph. D.
 ! date: 28 July 2022
-! summary: get inverse of square matrix from LU decomposition
+! summary: Get inverse of square matrix from LU decomposition
 
-INTERFACE
+INTERFACE Inv
   MODULE SUBROUTINE Inv_1(A, invA, IPIV, info)
     REAL(DFP), INTENT(IN) :: A(:, :)
     !! LU Decomposition
     REAL(DFP), INTENT(INOUT) :: invA(:, :)
     !! inverse of A
     INTEGER(I4B), INTENT(IN) :: IPIV(:)
-    !! returned from getLU
+    !! returned from GetLU
     INTEGER(I4B), OPTIONAL, INTENT(OUT) :: info
   END SUBROUTINE Inv_1
-END INTERFACE
-
-INTERFACE Inv
-  MODULE PROCEDURE Inv_1
 END INTERFACE Inv
 
 !----------------------------------------------------------------------------
-!                                                                    getInv
+!                                                                    GetInv
 !----------------------------------------------------------------------------
 
 !> author: Vikas Sharma, Ph. D.
 ! date: 28 July 2022
-! summary: get inverse of square matrix from LU decomposition
+! summary: Get inverse of square matrix from LU decomposition
 
-INTERFACE
+INTERFACE Inv
   MODULE SUBROUTINE Inv_2(A, IPIV, info)
     REAL(DFP), INTENT(INOUT) :: A(:, :)
     !! LU Decomposition, inverse will be returned in A
     INTEGER(I4B), INTENT(IN) :: IPIV(:)
-    !! returned from getLU
+    !! returned from GetLU
     INTEGER(I4B), OPTIONAL, INTENT(OUT) :: info
   END SUBROUTINE Inv_2
-END INTERFACE
-
-INTERFACE Inv
-  MODULE PROCEDURE Inv_2
 END INTERFACE Inv
 
 END MODULE GE_LUMethods
