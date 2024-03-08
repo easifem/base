a3e27c076d76b64dcc8c0fe325fc7bc07f88cad8
Vikas Sharma <vickysharma0812@gmail.com>
2024-03-08 23:11:33 +0900

    Update in Reference Hexahedron

--- src/submodules/Geometry/src/ReferenceHexahedron_Method@Methods.F90
+++ src/submodules/Geometry/src/ReferenceHexahedron_Method@Methods.F90
@@ -20,8 +20,17 @@
 ! summary: This module contains method for [[ReferenceHexahedron_]]
 
 SUBMODULE(ReferenceHexahedron_Method) Methods
-USE BaseMethod
+USE ReferenceElement_Method
+USE ApproxUtility
+USE InvUtility
+USE InputUtility
+USE StringUtility
+USE ArangeUtility
+USE ReferenceQuadrangle_Method, ONLY: RefQuadrangleCoord
+USE ReferencePrism_Method, ONLY: PolyhedronVolume3d
+
 IMPLICIT NONE
+
 CONTAINS
 
 !----------------------------------------------------------------------------
@@ -34,18 +43,22 @@ INTEGER(I4B) :: p1p2(2, 12), lloop(4, 6), vol(8, 1)
 REAL(DFP) :: unit_xij(3, 8), biunit_xij(3, 8)
 CALL DEALLOCATE (obj)
 
-p1p2 = EdgeConnectivity_Hexahedron( &
-  & baseInterpol="LAGRANGE",  &
-  & baseContinuity="H1")
+! p1p2 = EdgeConnectivity_Hexahedron( &
+!   & baseInterpol="LAGRANGE",  &
+!   & baseContinuity="H1")
 
-lloop = FacetConnectivity_Hexahedron( &
-  & baseInterpol="LAGRANGE",  &
-  & baseContinuity="H1")
+! lloop = FacetConnectivity_Hexahedron( &
+!   & baseInterpol="LAGRANGE",  &
+!   & baseContinuity="H1")
+
+CALL GetEdgeConnectivity_Hexahedron(con=p1p2, opt=2_I4B)
+CALL GetFaceConnectivity_Hexahedron(con=lloop, opt=2_I4B)
 
 vol(:, 1) = arange(1_I4B, 8_I4B)
 
 unit_xij = RefCoord_Hexahedron("UNIT")
 biunit_xij = RefCoord_Hexahedron("BIUNIT")
+
 IF (PRESENT(xij)) THEN
   obj%xij = xij(1:3, 1:8)
   IF (ALL(obj%xij(1:3, 1:8) .approxeq.unit_xij)) THEN
@@ -104,7 +117,8 @@ END PROCEDURE Initiate_ref_Hexahedron
 !----------------------------------------------------------------------------
 
 MODULE PROCEDURE reference_Hexahedron
-CALL Initiate(obj=obj, nsd=NSD, xij=xij, domainName=domainName)
+CALL Initiate_ref_Hexahedron(obj=obj, nsd=NSD, xij=xij,  &
+  & domainName=domainName)
 END PROCEDURE reference_Hexahedron
 
 !----------------------------------------------------------------------------
@@ -113,7 +127,8 @@ END PROCEDURE reference_Hexahedron
 
 MODULE PROCEDURE reference_Hexahedron_Pointer
 ALLOCATE (obj)
-CALL Initiate(obj=obj, nsd=NSD, xij=xij, domainName=domainName)
+CALL Initiate_ref_Hexahedron(obj=obj, nsd=NSD, xij=xij,  &
+  & domainName=domainName)
 END PROCEDURE reference_Hexahedron_Pointer
 
 !----------------------------------------------------------------------------
@@ -137,10 +152,8 @@ DO iFace = 1, 6
   Node0(iFace, 1:Order0(iFace)) = FM(iFace, 4:b)
 END DO
 CALL PolyhedronVolume3d(coord=XiJ(1:3, 1:8), &
-  & order_max=4, face_num=6,  &
-  & node=node0, node_num=8, &
-  & order=order0, &
-  & ans=ans)
+  & order_max=4, face_num=6, node=node0, node_num=8, &
+  & order=order0, ans=ans)
 END PROCEDURE Measure_Simplex_Hexahedron
 
 !----------------------------------------------------------------------------
@@ -148,6 +161,7 @@ END PROCEDURE Measure_Simplex_Hexahedron
 !----------------------------------------------------------------------------
 
 MODULE PROCEDURE Hexahedron_quality
+ans = 0.0_I4B
 ! TODO Implement Hexahedron_quality
 END PROCEDURE Hexahedron_quality
 
@@ -157,9 +171,87 @@ END PROCEDURE Hexahedron_quality
 
 MODULE PROCEDURE HexahedronVolume3D
 TYPE(ReferenceHexahedron_) :: refelem
-CALL Initiate(obj=refelem, nsd=3_I4B)
+CALL Initiate_ref_Hexahedron(obj=refelem, nsd=3_I4B)
 ans = Measure_Simplex_Hexahedron(refelem=refelem, xij=xij)
 CALL DEALLOCATE (refelem)
 END PROCEDURE HexahedronVolume3D
+
+!----------------------------------------------------------------------------
+!                                                       RefHexahedronCoord
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE RefHexahedronCoord
+REAL(DFP) :: one, mone
+CHARACTER(:), ALLOCATABLE :: astr
+
+astr = UpperCase(refHexahedron)
+
+SELECT CASE (astr)
+CASE ("UNIT")
+  one = 1.0_DFP
+  mone = 0.0_DFP
+CASE ("BIUNIT")
+  one = 1.0_DFP
+  mone = -1.0_DFP
+END SELECT
+
+astr = ""
+
+ans(3, 1:4) = mone
+ans(3, 5:8) = one
+ans(1:2, 1:4) = RefQuadrangleCoord(refHexahedron)
+ans(1:2, 5:8) = ans(1:2, 1:4)
+END PROCEDURE RefHexahedronCoord
+
+!----------------------------------------------------------------------------
+!                                            GetEdgeConnectivity_Hexahedron
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE GetEdgeConnectivity_Hexahedron
+con(1:2, 1) = [1, 2]
+con(1:2, 2) = [1, 4]
+con(1:2, 3) = [1, 5]
+con(1:2, 4) = [2, 3]
+con(1:2, 5) = [2, 6]
+con(1:2, 6) = [3, 4]
+con(1:2, 7) = [3, 7]
+con(1:2, 8) = [4, 8]
+con(1:2, 9) = [5, 6]
+con(1:2, 10) = [5, 8]
+con(1:2, 11) = [6, 7]
+con(1:2, 12) = [7, 8]
+END PROCEDURE GetEdgeConnectivity_Hexahedron
+
+!----------------------------------------------------------------------------
+!                                            GetFaceConnectivity_Hexahedron
+!----------------------------------------------------------------------------
+
+MODULE PROCEDURE GetFaceConnectivity_Hexahedron
+INTEGER(I4B) :: opt0
+
+opt0 = Input(default=1_I4B, option=opt)
+
+SELECT CASE (opt0)
+CASE (1_I4B)
+  con(1:4, 1) = [1, 2, 3, 4] ! back
+  con(1:4, 2) = [5, 6, 7, 8] ! front
+  con(1:4, 3) = [1, 4, 8, 5] ! left
+  con(1:4, 4) = [2, 3, 7, 6] ! right
+  con(1:4, 5) = [1, 2, 6, 5] ! bottom
+  con(1:4, 6) = [4, 3, 7, 8] ! top
+
+CASE (2_I4B)
+  con(1:4, 1) = [1, 4, 3, 2] ! back
+  con(1:4, 2) = [5, 6, 7, 8] ! front
+  con(1:4, 3) = [1, 5, 8, 4] ! left
+  con(1:4, 4) = [2, 3, 7, 6] ! right
+  con(1:4, 5) = [1, 2, 6, 5] ! bottom
+  con(1:4, 6) = [3, 4, 8, 7] ! top
+END SELECT
+END PROCEDURE GetFaceConnectivity_Hexahedron
+
+!----------------------------------------------------------------------------
+!
+!----------------------------------------------------------------------------
 
 END SUBMODULE Methods
