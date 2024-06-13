11697d3b8bcba6433a62c0b3c7e75789d6d7a932
Vikas Sharma <vickysharma0812@gmail.com>
2024-06-13 18:13:05 +0900

    updates in fevariable

---
+++ src/submodules/FEVariable/src/include/matrix_space_time2.F90
@@ -0,0 +1,10 @@
+obj%len = SIZE(val)
+obj%capacity = CAPACITY_EXPAND_FACTOR * obj%len
+ALLOCATE (obj%val(obj%capacity))
+
+obj%val(1:obj%len) = val(1:obj%len)
+
+obj%s(1:4) = s(1:4)
+obj%defineOn = _DEFINEON_
+obj%rank = Matrix
+obj%varType = SpaceTime
