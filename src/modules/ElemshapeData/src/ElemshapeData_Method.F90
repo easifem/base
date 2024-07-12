81c013c793d1425e71fbd26f0bcbf918e0221c24
Vikas Sharma <vickysharma0812@gmail.com>
2024-07-12 18:48:05 +0900

    update in elemeshape data

--- src/modules/ElemshapeData/src/ElemshapeData_Method.F90
+++ src/modules/ElemshapeData/src/ElemshapeData_Method.F90
@@ -20,10 +20,14 @@ USE ElemshapeData_DGMethods
 USE ElemshapeData_DivergenceMethods
 USE ElemshapeData_GetMethods
 USE ElemshapeData_GradientMethods
+
 USE ElemshapeData_H1Methods
-USE ElemshapeData_Lagrange
 USE ElemshapeData_HCurlMethods
 USE ElemshapeData_HDivMethods
+
+USE ElemshapeData_Lagrange
+USE ElemshapeData_Hierarchical
+
 USE ElemshapeData_HRGNParamMethods
 USE ElemshapeData_HRQIParamMethods
 USE ElemshapeData_HminHmaxMethods
