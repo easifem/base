214855fba04c9b020893127754337c9c1bc3d13e
Vikas Sharma <vickysharma0812@gmail.com>
2024-07-16 10:38:14 +0900

    update in facet matrix methods

--- src/submodules/FacetMatrix/src/FacetMatrix_Method@FacetMatrix13Methods.F90
+++ src/submodules/FacetMatrix/src/FacetMatrix_Method@FacetMatrix13Methods.F90
@@ -26,46 +26,46 @@ CONTAINS
 
 MODULE PROCEDURE FacetMatrix13_1
   !!
-  REAL( DFP ), ALLOCATABLE :: realval( : ), masterC1( :, : ),  &
-    & m4( :, :, :, : )
-  INTEGER( I4B ) :: ips, ii, nips, nns1, nns2, nsd, nsd1, nsd2, jj
+REAL(DFP), ALLOCATABLE :: realval(:), masterC1(:, :),  &
+  & m4(:, :, :, :)
+INTEGER(I4B) :: ips, ii, nips, nns1, nns2, nsd, nsd1, nsd2, jj
   !!
-  nns1 = SIZE( elemsd%dNdXt, 1 )
-  nns2 = SIZE( elemsd%N, 1 )
-  nsd = SIZE( elemsd%dNdXt, 2 )
-  nips = SIZE( elemsd%dNdXt, 3 )
+nns1 = SIZE(elemsd%dNdXt, 1)
+nns2 = SIZE(elemsd%N, 1)
+nsd = SIZE(elemsd%dNdXt, 2)
+nips = SIZE(elemsd%dNdXt, 3)
   !!
-  IF( opt .EQ. 1 ) THEN
-    nsd1 = nsd
-    nsd2 = 1
-  ELSE
-    nsd1 = 1
-    nsd2 = nsd
-  END IF
+IF (opt .EQ. 1) THEN
+  nsd1 = nsd
+  nsd2 = 1
+ELSE
+  nsd1 = 1
+  nsd2 = nsd
+END IF
   !!
-  CALL Reallocate(m4, nns1, nns2, nsd1, nsd2)
+CALL Reallocate(m4, nns1, nns2, nsd1, nsd2)
   !!
-  CALL getProjectionOfdNdXt( &
-    & obj=elemsd, &
-    & cdNdXt=masterC1, &
-    & val=elemsd%normal )
+CALL getProjectionOfdNdXt( &
+  & obj=elemsd, &
+  & cdNdXt=masterC1, &
+  & val=elemsd%normal)
   !!
-  realval = elemsd%js * elemsd%ws * elemsd%thickness
+realval = elemsd%js * elemsd%ws * elemsd%thickness
   !!
-  DO ips = 1, nips
-    DO jj = 1, nsd2
-      DO ii = 1, nsd1
-        m4( :, :, ii, jj ) = m4( :, :, ii, jj ) &
-          & + realval( ips ) * OUTERPROD( &
-          & masterC1( :, ips ) * elemsd%normal( ii+jj-1, ips ), &
-          & elemsd%N( :, ips ) )
-      END DO
+DO ips = 1, nips
+  DO jj = 1, nsd2
+    DO ii = 1, nsd1
+      m4(:, :, ii, jj) = m4(:, :, ii, jj) &
+        & + realval(ips) * OUTERPROD( &
+        & masterC1(:, ips) * elemsd%normal(ii + jj - 1, ips), &
+        & elemsd%N(:, ips))
     END DO
   END DO
+END DO
   !!
-  CALL Convert( from=m4, to=ans )
+CALL Convert(from=m4, to=ans)
   !!
-  DEALLOCATE( m4, realval, masterC1 )
+DEALLOCATE (m4, realval, masterC1)
   !!
 END PROCEDURE FacetMatrix13_1
 
@@ -75,46 +75,46 @@ END PROCEDURE FacetMatrix13_1
 
 MODULE PROCEDURE FacetMatrix13_2
   !!
-  REAL( DFP ), ALLOCATABLE :: realval( : ), masterC1( :, : ), &
-    & m4( :, :, :, : )
-  INTEGER( I4B ) :: ips, ii, nips, nns1, nns2, nsd, nsd1, nsd2, jj
+REAL(DFP), ALLOCATABLE :: realval(:), masterC1(:, :), &
+  & m4(:, :, :, :)
+INTEGER(I4B) :: ips, ii, nips, nns1, nns2, nsd, nsd1, nsd2, jj
   !!
-  nns1 = SIZE( elemsd%dNdXt, 1 )
-  nns2 = SIZE( elemsd%N, 1 )
-  nsd = SIZE( elemsd%dNdXt, 2 )
-  nips = SIZE( elemsd%dNdXt, 3 )
+nns1 = SIZE(elemsd%dNdXt, 1)
+nns2 = SIZE(elemsd%N, 1)
+nsd = SIZE(elemsd%dNdXt, 2)
+nips = SIZE(elemsd%dNdXt, 3)
   !!
-  IF( opt .EQ. 1 ) THEN
-    nsd1 = nsd
-    nsd2 = 1
-  ELSE
-    nsd1 = 1
-    nsd2 = nsd
-  END IF
+IF (opt .EQ. 1) THEN
+  nsd1 = nsd
+  nsd2 = 1
+ELSE
+  nsd1 = 1
+  nsd2 = nsd
+END IF
   !!
-  CALL Reallocate(m4, nns1, nns2, nsd1, nsd2)
+CALL Reallocate(m4, nns1, nns2, nsd1, nsd2)
   !!
-  CALL getProjectionOfdNdXt( &
-    & obj=elemsd, &
-    & cdNdXt=masterC1, &
-    & val=elemsd%normal )
+CALL getProjectionOfdNdXt( &
+  & obj=elemsd, &
+  & cdNdXt=masterC1, &
+  & val=elemsd%normal)
   !!
-  realval = elemsd%js * elemsd%ws * elemsd%thickness * mu
+realval = elemsd%js * elemsd%ws * elemsd%thickness * mu
   !!
-  DO ips = 1, nips
-    DO jj = 1, nsd2
-      DO ii = 1, nsd1
-        m4( :, :, ii, jj ) = m4( :, :, ii, jj ) &
-          & + realval( ips ) * OUTERPROD( &
-          & masterC1( :, ips ) * elemsd%normal( ii+jj-1, ips ), &
-          & elemsd%N( :, ips ) )
-      END DO
+DO ips = 1, nips
+  DO jj = 1, nsd2
+    DO ii = 1, nsd1
+      m4(:, :, ii, jj) = m4(:, :, ii, jj) &
+        & + realval(ips) * OUTERPROD( &
+        & masterC1(:, ips) * elemsd%normal(ii + jj - 1, ips), &
+        & elemsd%N(:, ips))
     END DO
   END DO
+END DO
   !!
-  CALL Convert( from=m4, to=ans )
+CALL Convert(from=m4, to=ans)
   !!
-  DEALLOCATE( m4, realval, masterC1 )
+DEALLOCATE (m4, realval, masterC1)
   !!
 END PROCEDURE FacetMatrix13_2
 
@@ -124,51 +124,50 @@ END PROCEDURE FacetMatrix13_2
 
 MODULE PROCEDURE FacetMatrix13_3
   !!
-  REAL( DFP ), ALLOCATABLE :: realval( : ), masterC1( :, : ), &
-    & m4( :, :, :, : ), taubar( : )
-  INTEGER( I4B ) :: ips, ii, nips, nns1, nns2, nsd, nsd1, nsd2, jj
+REAL(DFP), ALLOCATABLE :: realval(:), masterC1(:, :), &
+  & m4(:, :, :, :), taubar(:)
+INTEGER(I4B) :: ips, ii, nips, nns1, nns2, nsd, nsd1, nsd2, jj
   !!
-  nns1 = SIZE( elemsd%dNdXt, 1 )
-  nns2 = SIZE( elemsd%N, 1 )
-  nsd = SIZE( elemsd%dNdXt, 2 )
-  nips = SIZE( elemsd%dNdXt, 3 )
+nns1 = SIZE(elemsd%dNdXt, 1)
+nns2 = SIZE(elemsd%N, 1)
+nsd = SIZE(elemsd%dNdXt, 2)
+nips = SIZE(elemsd%dNdXt, 3)
   !!
-  IF( opt .EQ. 1 ) THEN
-    nsd1 = nsd
-    nsd2 = 1
-  ELSE
-    nsd1 = 1
-    nsd2 = nsd
-  END IF
+IF (opt .EQ. 1) THEN
+  nsd1 = nsd
+  nsd2 = 1
+ELSE
+  nsd1 = 1
+  nsd2 = nsd
+END IF
   !!
-  CALL Reallocate(m4, nns1, nns2, nsd1, nsd2)
+CALL Reallocate(m4, nns1, nns2, nsd1, nsd2)
   !!
-  CALL getProjectionOfdNdXt( &
-  & obj=elemsd, &
-  & cdNdXt=masterC1, &
-  & val=elemsd%normal )
+CALL getProjectionOfdNdXt( &
+& obj=elemsd, &
+& cdNdXt=masterC1, &
+& val=elemsd%normal)
   !!
-  CALL getInterpolation(obj=elemsd, Interpol=taubar, val=tauvar)
+CALL getInterpolation(obj=elemsd, Interpol=taubar, val=tauvar)
   !!
-  realval = elemsd%js * elemsd%ws * elemsd%thickness * mu* taubar
+realval = elemsd%js * elemsd%ws * elemsd%thickness * mu * taubar
   !!
-  DO ips = 1, nips
-    DO jj = 1, nsd2
-      DO ii = 1, nsd1
-        m4( :, :, ii, jj ) = m4( :, :, ii, jj ) &
-          & + realval( ips ) * OUTERPROD( &
-          & masterC1( :, ips ) * elemsd%normal( ii+jj-1, ips ), &
-          & elemsd%N( :, ips ) )
-      END DO
+DO ips = 1, nips
+  DO jj = 1, nsd2
+    DO ii = 1, nsd1
+      m4(:, :, ii, jj) = m4(:, :, ii, jj) &
+        & + realval(ips) * OUTERPROD( &
+        & masterC1(:, ips) * elemsd%normal(ii + jj - 1, ips), &
+        & elemsd%N(:, ips))
     END DO
   END DO
+END DO
   !!
-  CALL Convert( from=m4, to=ans )
+CALL Convert(from=m4, to=ans)
   !!
-  DEALLOCATE( m4, realval, masterC1, taubar )
+DEALLOCATE (m4, realval, masterC1, taubar)
   !!
 END PROCEDURE FacetMatrix13_3
-
 
 !----------------------------------------------------------------------------
 !                                                              FacetMatrix13
@@ -176,48 +175,48 @@ END PROCEDURE FacetMatrix13_3
 
 MODULE PROCEDURE FacetMatrix13_4
   !!
-  REAL( DFP ), ALLOCATABLE :: realval( : ), masterC1( :, : ), &
-    & m4( :, :, :, : ), mubar( : )
-  INTEGER( I4B ) :: ips, ii, nips, nns1, nns2, nsd, nsd1, nsd2, jj
+REAL(DFP), ALLOCATABLE :: realval(:), masterC1(:, :), &
+  & m4(:, :, :, :), mubar(:)
+INTEGER(I4B) :: ips, ii, nips, nns1, nns2, nsd, nsd1, nsd2, jj
   !!
-  nns1 = SIZE( elemsd%dNdXt, 1 )
-  nns2 = SIZE( elemsd%N, 1 )
-  nsd = SIZE( elemsd%dNdXt, 2 )
-  nips = SIZE( elemsd%dNdXt, 3 )
+nns1 = SIZE(elemsd%dNdXt, 1)
+nns2 = SIZE(elemsd%N, 1)
+nsd = SIZE(elemsd%dNdXt, 2)
+nips = SIZE(elemsd%dNdXt, 3)
   !!
-  IF( opt .EQ. 1 ) THEN
-    nsd1 = nsd
-    nsd2 = 1
-  ELSE
-    nsd1 = 1
-    nsd2 = nsd
-  END IF
+IF (opt .EQ. 1) THEN
+  nsd1 = nsd
+  nsd2 = 1
+ELSE
+  nsd1 = 1
+  nsd2 = nsd
+END IF
   !!
-  CALL Reallocate(m4, nns1, nns2, nsd1, nsd2)
+CALL Reallocate(m4, nns1, nns2, nsd1, nsd2)
   !!
-  CALL getProjectionOfdNdXt( &
-    & obj=elemsd, &
-    & cdNdXt=masterC1, &
-    & val=elemsd%normal )
+CALL getProjectionOfdNdXt( &
+  & obj=elemsd, &
+  & cdNdXt=masterC1, &
+  & val=elemsd%normal)
   !!
-  CALL getInterpolation(obj=elemsd, Interpol=mubar, val=mu)
+CALL getInterpolation(obj=elemsd, Interpol=mubar, val=mu)
   !!
-  realval = elemsd%js * elemsd%ws * elemsd%thickness * mubar
+realval = elemsd%js * elemsd%ws * elemsd%thickness * mubar
   !!
-  DO ips = 1, nips
-    DO jj = 1, nsd2
-      DO ii = 1, nsd1
-        m4( :, :, ii, jj ) = m4( :, :, ii, jj ) &
-          & + realval( ips ) * OUTERPROD( &
-          & masterC1( :, ips ) * elemsd%normal( ii+jj-1, ips ), &
-          & elemsd%N( :, ips ) )
-      END DO
+DO ips = 1, nips
+  DO jj = 1, nsd2
+    DO ii = 1, nsd1
+      m4(:, :, ii, jj) = m4(:, :, ii, jj) &
+        & + realval(ips) * OUTERPROD( &
+        & masterC1(:, ips) * elemsd%normal(ii + jj - 1, ips), &
+        & elemsd%N(:, ips))
     END DO
   END DO
+END DO
   !!
-  CALL Convert( from=m4, to=ans )
+CALL Convert(from=m4, to=ans)
   !!
-  DEALLOCATE( m4, realval, masterC1, mubar )
+DEALLOCATE (m4, realval, masterC1, mubar)
   !!
 END PROCEDURE FacetMatrix13_4
 
@@ -227,49 +226,49 @@ END PROCEDURE FacetMatrix13_4
 
 MODULE PROCEDURE FacetMatrix13_5
   !!
-  REAL( DFP ), ALLOCATABLE :: realval( : ), masterC1( :, : ), &
-    & m4( :, :, :, : ), mubar( : ), taubar( : )
-  INTEGER( I4B ) :: ips, ii, nips, nns1, nns2, nsd, nsd1, nsd2, jj
+REAL(DFP), ALLOCATABLE :: realval(:), masterC1(:, :), &
+  & m4(:, :, :, :), mubar(:), taubar(:)
+INTEGER(I4B) :: ips, ii, nips, nns1, nns2, nsd, nsd1, nsd2, jj
   !!
-  nns1 = SIZE( elemsd%dNdXt, 1 )
-  nns2 = SIZE( elemsd%N, 1 )
-  nsd = SIZE( elemsd%dNdXt, 2 )
-  nips = SIZE( elemsd%dNdXt, 3 )
+nns1 = SIZE(elemsd%dNdXt, 1)
+nns2 = SIZE(elemsd%N, 1)
+nsd = SIZE(elemsd%dNdXt, 2)
+nips = SIZE(elemsd%dNdXt, 3)
   !!
-  IF( opt .EQ. 1 ) THEN
-    nsd1 = nsd
-    nsd2 = 1
-  ELSE
-    nsd1 = 1
-    nsd2 = nsd
-  END IF
+IF (opt .EQ. 1) THEN
+  nsd1 = nsd
+  nsd2 = 1
+ELSE
+  nsd1 = 1
+  nsd2 = nsd
+END IF
   !!
-  CALL Reallocate(m4, nns1, nns2, nsd1, nsd2)
+CALL Reallocate(m4, nns1, nns2, nsd1, nsd2)
   !!
-  CALL getProjectionOfdNdXt( &
-  & obj=elemsd, &
-  & cdNdXt=masterC1, &
-  & val=elemsd%normal )
+CALL getProjectionOfdNdXt( &
+& obj=elemsd, &
+& cdNdXt=masterC1, &
+& val=elemsd%normal)
   !!
-  CALL getInterpolation(obj=elemsd, Interpol=mubar, val=mu)
-  CALL getInterpolation(obj=elemsd, Interpol=taubar, val=tauvar)
+CALL getInterpolation(obj=elemsd, Interpol=mubar, val=mu)
+CALL getInterpolation(obj=elemsd, Interpol=taubar, val=tauvar)
   !!
-  realval = elemsd%js * elemsd%ws * elemsd%thickness * mubar * taubar
+realval = elemsd%js * elemsd%ws * elemsd%thickness * mubar * taubar
   !!
-  DO ips = 1, nips
-    DO jj = 1, nsd2
-      DO ii = 1, nsd1
-        m4( :, :, ii, jj ) = m4( :, :, ii, jj ) &
-          & + realval( ips ) * OUTERPROD( &
-          & masterC1( :, ips ) * elemsd%normal( ii+jj-1, ips ), &
-          & elemsd%N( :, ips ) )
-      END DO
+DO ips = 1, nips
+  DO jj = 1, nsd2
+    DO ii = 1, nsd1
+      m4(:, :, ii, jj) = m4(:, :, ii, jj) &
+        & + realval(ips) * OUTERPROD( &
+        & masterC1(:, ips) * elemsd%normal(ii + jj - 1, ips), &
+        & elemsd%N(:, ips))
     END DO
   END DO
+END DO
   !!
-  CALL Convert( from=m4, to=ans )
+CALL Convert(from=m4, to=ans)
   !!
-  DEALLOCATE( m4, realval, masterC1, mubar, taubar )
+DEALLOCATE (m4, realval, masterC1, mubar, taubar)
   !!
 END PROCEDURE FacetMatrix13_5
 
